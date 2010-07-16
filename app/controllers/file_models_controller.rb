ebada98bb7a0390deb2a23f4653a5e413bc31e87
Franco Catena <francocatena@gmail.com>
2010-07-16 12:41:08 -0300

    Corrección en un problema de descarga en IE
    Internet Explorer no funcionaba correctamente si se establecía la cabecera Cache-control a un valor 'no-cache' y/o 'no-store' (http://support.microsoft.com/kb/323308) (refs #32)

--- app/controllers/file_models_controller.rb
+++ app/controllers/file_models_controller.rb
@@ -8,7 +8,12 @@ class FileModelsController < ApplicationController
     base_regexp = %r(^#{Regexp.escape(PRIVATE_PATH)})
 
     if file_name =~ base_regexp && File.file?(file_name)
-      send_file file_name, :x_sendfile => (Rails.env == 'production')
+      response.headers['Cache-Control'] = 'private'
+      extname = File.extname(file_name)[1..-1]
+      mime_type = Mime::Type.lookup_by_extension(extname)
+
+      send_file file_name, :x_sendfile => (Rails.env == 'production'),
+        :url_based_filename => true, :type => mime_type
       redirect = false
     end
 
