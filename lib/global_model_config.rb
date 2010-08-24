7e84b4d5bfd018a7ba53e906968bf6c836bf6bb6
Franco Catena <francocatena@gmail.com>
2010-08-24 13:21:53 -0300

    Ahora GlobalModelConfig es Thread Safe
    Se reemplazó el uso de atributos de clase por Thread.current

--- lib/global_model_config.rb
+++ lib/global_model_config.rb
@@ -1,23 +1,26 @@
 module GlobalModelConfig
-  @@current_organization_id = nil
-
   def self.included(base)
     base.before_filter :set_current_organization_id
   end
 
   def self.current_organization_id
-    @@current_organization_id.respond_to?(:call) ?
-      @@current_organization_id.call : @@current_organization_id
+    GlobalModelConfig.config_store[:organization_id].respond_to?(:call) ?
+      GlobalModelConfig.config_store[:organization_id].call :
+      GlobalModelConfig.config_store[:organization_id]
   end
 
   def self.current_organization_id=(value)
-    @@current_organization_id = value
+    GlobalModelConfig.config_store[:organization_id] = value
+  end
+
+  def self.config_store
+    Thread.current[:mawida] ||= {}
   end
 
   private
 
   def set_current_organization_id
-    @@current_organization_id = lambda {
+    GlobalModelConfig.config_store[:organization_id] = lambda {
       self.respond_to?(:current_organization) ? self.current_organization : nil
     }
   end
