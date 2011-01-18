afd711e2e17d86f7957b344e27b208a959e14d2d
Franco Catena <francocatena@gmail.com>
2011-01-18 14:03:03 -0300

    Correcciones en la configuración de correos
    La configuración de correos no se cargaba bien debido al class_eval

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,13 +1,14 @@
-MawidaApp::Application.class_exec do
-  config.action_mailer.default_url_options = { :host => URL_HOST }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => 'mawidaweb.com.ar',
-    :domain => 'mawidaweb.com.ar',
-    :port => 25,
-    :user_name => 'soporte@mawidaweb.com.ar',
-    :password => '5rdxcft6',
-    :authentication => :plain
-  }
-end
+MawidaApp::Application.config.action_mailer.default_url_options = {
+  :host => URL_HOST
+}
+MawidaApp::Application.config.action_mailer.raise_delivery_errors =
+  !Rails.env.production?
+MawidaApp::Application.config.action_mailer.delivery_method = :smtp
+MawidaApp::Application.config.action_mailer.smtp_settings = {
+  :address => 'mawidaweb.com.ar',
+  :domain => 'mawidaweb.com.ar',
+  :port => 25,
+  :user_name => 'soporte@mawidaweb.com.ar',
+  :password => '5rdxcft6',
+  :authentication => :plain
+}
