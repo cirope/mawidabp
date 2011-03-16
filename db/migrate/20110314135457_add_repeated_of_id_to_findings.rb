f28e3c1f342cb74593d189619779c5f15fad78cb
Franco Catena <francocatena@gmail.com>
2011-03-16 14:44:14 -0300

    Agregado de clave foranea en 20110314135457

--- db/migrate/20110314135457_add_repeated_of_id_to_findings.rb
+++ db/migrate/20110314135457_add_repeated_of_id_to_findings.rb
@@ -3,10 +3,13 @@ class AddRepeatedOfIdToFindings < ActiveRecord::Migration
     add_column :findings, :repeated_of_id, :integer
 
     add_index :findings, :repeated_of_id
+    add_foreign_key :findings, :findings, :column => :repeated_of_id,
+      :dependent => :restrict
   end
 
   def self.down
     remove_index :findings, :column => :repeated_of_id
+    remove_foreign_key :findings, :column => :repeated_of_id
 
     remove_column :findings, :repeated_of_id
   end
