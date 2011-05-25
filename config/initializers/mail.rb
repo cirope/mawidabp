d88d1a074176d5941d621cc78a502020a867b4a7
Franco Catena <francocatena@gmail.com>
2011-05-25 15:56:46 -0300

    Actualización de correo a dirección mawida.com.ar

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -5,10 +5,10 @@ MawidaApp::Application.config.action_mailer.raise_delivery_errors =
   !Rails.env.production?
 MawidaApp::Application.config.action_mailer.delivery_method = :smtp
 MawidaApp::Application.config.action_mailer.smtp_settings = {
-  :address => 'mawidaweb.com.ar',
-  :domain => 'mawidaweb.com.ar',
+  :address => 'mawida.com.ar',
+  :domain => 'mawida.com.ar',
   :port => 25,
-  :user_name => 'soporte@mawidaweb.com.ar',
+  :user_name => 'soporte@mawida.com.ar',
   :password => APP_CONFIG['smtp_password'],
   :authentication => :plain
 }
