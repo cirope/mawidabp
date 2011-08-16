33cb158b1571da4a9fd5035ad7cf1b17c5c3f9de
Franco Catena <francocatena@gmail.com>
2011-08-16 09:29:26 -0300

    Mejoras en la paginación

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -166,17 +166,17 @@ module ApplicationHelper
   #
   # * _objects_:: Objetos con los que se genera la lista paginada
   def pagination_links(objects)
-    previous_label = "&laquo; #{t :'label.previous'}".html_safe
-    next_label = "#{t :'label.next'} &raquo;".html_safe
+    result = will_paginate objects, :inner_window => 1, :outer_window => 1
 
-    result = will_paginate objects, :previous_label => previous_label,
-      :next_label => next_label, :inner_window => 1, :outer_window => 1
-
-    result ||= content_tag(:div, content_tag(:span, previous_label,
+    result ||= content_tag(
+      :div,
+        content_tag(:span, raw(t('will_paginate.previous_label')),
         :class => 'disabled prev_page') +
-        content_tag(:em, 1) + content_tag(:span, next_label,
+        content_tag(:em, 1) +
+        content_tag(:span, raw(t('will_paginate.next_label')),
         :class => 'disabled next_page'),
-      :class => :pagination)
+      :class => :pagination
+    )
 
     result
   end
