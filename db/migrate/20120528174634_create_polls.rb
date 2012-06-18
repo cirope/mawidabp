90152aa2eaa36be9503647b597231419e7d81edf
Martín Vega <martin.vega7@gmail.com>
2012-06-18 14:34:41 -0300

    Valor por defecto en contestada de encuestas

--- db/migrate/20120528174634_create_polls.rb
+++ db/migrate/20120528174634_create_polls.rb
@@ -2,7 +2,7 @@ class CreatePolls < ActiveRecord::Migration
   def change
     create_table :polls do |t|
       t.text :comments
-      t.boolean :answered
+      t.boolean :answered, :default => false
       t.integer :lock_version, :default => 0
       t.references :user
       t.references :questionnaire
