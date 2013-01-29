f9cf67899ae2b1cea36fc53dba2a6ba9d91955f7
Martín Vega <martin.vega7@gmail.com>
2013-01-29 13:40:54 -0300

    Observaciones de riesgo alto - Conclusión. Resuelve #17

--- app/controllers/modules/conclusion_reports/conclusion_high_risk_reports.rb
+++ app/controllers/modules/conclusion_reports/conclusion_high_risk_reports.rb
@@ -14,7 +14,7 @@ module ConclusionHighRiskReports
     conclusion_reviews = ConclusionFinalReview.list_all_by_date(
       @from_date, @to_date
     ).notorious(true)
-    
+
     if params[:high_risk_weaknesses_report]
       unless params[:high_risk_weaknesses_report][:business_unit_type].blank?
         @selected_business_unit = BusinessUnitType.find(
@@ -57,7 +57,7 @@ module ConclusionHighRiskReports
           high_risk_weaknesses = []
           weaknesses = c_r.review.final_weaknesses.with_highest_risk.
             with_pending_status_for_report
-          
+
           weaknesses.each do |w|
             audited = w.users.select(&:audited?).map do |u|
               w.process_owners.include?(u) ?
@@ -80,11 +80,11 @@ module ConclusionHighRiskReports
           end
 
           unless high_risk_weaknesses.blank?
-            column_data << {
-              'business_unit_report_name' => c_r.review.business_unit.name,
-              'score' => c_r.review.reload.score_text,
-              'high_risk_weaknesses' => high_risk_weaknesses
-            }
+            column_data << [
+              c_r.review.business_unit.name,
+              c_r.review.reload.score_text,
+              high_risk_weaknesses
+            ]
           end
         end
 
@@ -108,17 +108,17 @@ module ConclusionHighRiskReports
   def create_high_risk_weaknesses_report
     self.high_risk_weaknesses_report
 
-    pdf = PDF::Writer.create_generic_pdf :landscape
+    pdf = Prawn::Document.create_generic_pdf :landscape
 
     pdf.add_generic_report_header @auth_organization
 
     pdf.add_title params[:report_title], PDF_FONT_SIZE, :center
 
-    pdf.move_pointer PDF_FONT_SIZE
+    pdf.move_down PDF_FONT_SIZE
 
     pdf.add_title params[:report_subtitle], PDF_FONT_SIZE, :center
 
-    pdf.move_pointer PDF_FONT_SIZE
+    pdf.move_down PDF_FONT_SIZE
 
     pdf.add_description_item(
       t('conclusion_committee_report.period.title'),
@@ -128,23 +128,20 @@ module ConclusionHighRiskReports
 
     @periods.each do |period|
       unless @notorious_reviews[period].blank?
-        pdf.move_pointer PDF_FONT_SIZE
+        pdf.move_down PDF_FONT_SIZE
         pdf.add_title "#{Period.model_name.human}: #{period.inspect}",
-          (PDF_FONT_SIZE * 1.25).round, :justify
+          (PDF_FONT_SIZE * 1.25).round, :left
 
-        pdf.move_pointer PDF_FONT_SIZE
+        pdf.move_down PDF_FONT_SIZE
 
         @notorious_reviews[period].each do |data|
           columns = data[:columns]
-          column_data = []
+          column_data, column_headers, column_widths = [], [], []
 
-          @column_order.each do |col_name|
-            columns[col_name] = PDF::SimpleTable::Column.new(col_name) do |column|
-              column.heading = columns[col_name].first
-              column.width = pdf.percent_width columns[col_name].last
-            end
+          @column_order.each do |order|
+            column_headers << columns[order].first
+            column_widths << columns[order].last
           end
-
           if !data[:external] && !@internal_title_showed
             title = t('conclusion_committee_report.high_risk_weaknesses_report.internal_audit_weaknesses')
             @internal_title_showed = true
@@ -154,57 +151,39 @@ module ConclusionHighRiskReports
           end
 
           if title
-            pdf.move_pointer PDF_FONT_SIZE * 2
+            pdf.move_down PDF_FONT_SIZE * 2
             pdf.add_title title, (PDF_FONT_SIZE * 1.25).round, :center
           end
 
           pdf.add_subtitle data[:name], PDF_FONT_SIZE, PDF_FONT_SIZE
 
-          data[:column_data].each do |row|
-            new_row = {}
-
-            row.each do |column_name, column_content|
-              new_row[column_name] = column_content.kind_of?(Array) ?
-                column_content.join("\n\n").to_iso :
-                column_content.to_iso
-            end
-
-            column_data << new_row
-          end
-
+          column_data = data[:column_data]
           unless column_data.blank?
-            PDF::SimpleTable.new do |table|
-              table.width = pdf.page_usable_width
-              table.columns = columns
-              table.data = column_data.sort do |row1, row2|
-                row1['score'].match(/(\d+)%/)[0].to_i <=>
-                  row2['score'].match(/(\d+)%/)[0].to_i
+            pdf.font_size((PDF_FONT_SIZE * 0.75).round) do
+              column_data.each do |col_data|
+                column_headers.each_with_index do |header, i|
+                  data = col_data[i].kind_of?(Array) ? "\n\n #{col_data[i]}" : col_data[i]
+                  pdf.text "<b>#{header.upcase}</b>: #{data}", :inline_format => true
+                  pdf.move_down PDF_FONT_SIZE
+                end
               end
-              table.column_order = @column_order
-              table.split_rows = true
-              table.row_gap = PDF_FONT_SIZE
-              table.font_size = (PDF_FONT_SIZE * 0.75).round
-              table.shade_color = Color::RGB.from_percentage(95, 95, 95)
-              table.shade_heading_color = Color::RGB.from_percentage(85, 85, 85)
-              table.heading_font_size = PDF_FONT_SIZE
-              table.shade_headings = true
-              table.position = :left
-              table.orientation = :right
-              table.render_on pdf
             end
           else
             pdf.text(
-              t('conclusion_committee_report.high_risk_weaknesses_report.without_audits_in_the_period'))
+              t('conclusion_committee_report.high_risk_weaknesses_report.without_audits_in_the_period'),
+              :style => :italic
+            )
           end
         end
       end
     end
 
     unless @filters.empty?
-      pdf.move_pointer PDF_FONT_SIZE
+      pdf.move_down PDF_FONT_SIZE
       pdf.text t('conclusion_committee_report.applied_filters',
         :filters => @filters.to_sentence, :count => @filters.size),
-        :font_size => (PDF_FONT_SIZE * 0.75).round, :justification => :full
+        :font_size => (PDF_FONT_SIZE * 0.75).round, :justification => :full,
+        :inline_format => true
     end
 
     pdf.custom_save_as(
@@ -212,7 +191,7 @@ module ConclusionHighRiskReports
         :from_date => @from_date.to_formatted_s(:db),
         :to_date => @to_date.to_formatted_s(:db)), 'high_risk_weaknesses_report', 0)
 
-    redirect_to PDF::Writer.relative_path(
+    redirect_to Prawn::Document.relative_path(
       t('conclusion_committee_report.high_risk_weaknesses_report.pdf_name',
         :from_date => @from_date.to_formatted_s(:db),
         :to_date => @to_date.to_formatted_s(:db)), 'high_risk_weaknesses_report', 0)
@@ -232,7 +211,7 @@ module ConclusionHighRiskReports
     conclusion_reviews = ConclusionFinalReview.list_all_by_final_solution_date(
       @from_date, @to_date
     )
-    
+
     if params[:fixed_weaknesses_report]
       unless params[:fixed_weaknesses_report][:business_unit_type].blank?
         @selected_business_unit = BusinessUnitType.find(
@@ -325,17 +304,17 @@ module ConclusionHighRiskReports
   def create_fixed_weaknesses_report
     self.fixed_weaknesses_report
 
-    pdf = PDF::Writer.create_generic_pdf :landscape
+    pdf = Prawn::Document.create_generic_pdf :landscape
 
     pdf.add_generic_report_header @auth_organization
 
     pdf.add_title params[:report_title], PDF_FONT_SIZE, :center
 
-    pdf.move_pointer PDF_FONT_SIZE
+    pdf.move_down PDF_FONT_SIZE
 
     pdf.add_title params[:report_subtitle], PDF_FONT_SIZE, :center
 
-    pdf.move_pointer PDF_FONT_SIZE
+    pdf.move_down PDF_FONT_SIZE
 
     pdf.add_description_item(
       t('conclusion_committee_report.period.title'),
@@ -345,21 +324,19 @@ module ConclusionHighRiskReports
 
     @periods.each do |period|
       unless @reviews[period].blank?
-        pdf.move_pointer PDF_FONT_SIZE
+        pdf.move_down PDF_FONT_SIZE
         pdf.add_title "#{Period.model_name.human}: #{period.inspect}",
           (PDF_FONT_SIZE * 1.25).round, :justify
 
-        pdf.move_pointer PDF_FONT_SIZE
+        pdf.move_down PDF_FONT_SIZE
 
         @reviews[period].each do |data|
           columns = data[:columns]
-          column_data = []
+          column_data, column_headers, column_widths = [], [], []
 
           @column_order.each do |col_name|
-            columns[col_name] = PDF::SimpleTable::Column.new(col_name) do |column|
-              column.heading = columns[col_name].first
-              column.width = pdf.percent_width columns[col_name].last
-            end
+            column_headers = col_name.first
+            column_widths = pdf.percent_width col_name.last
           end
 
           if !data[:external] && !@internal_title_showed
@@ -371,43 +348,34 @@ module ConclusionHighRiskReports
           end
 
           if title
-            pdf.move_pointer PDF_FONT_SIZE * 2
+            pdf.move_down PDF_FONT_SIZE * 2
             pdf.add_title title, (PDF_FONT_SIZE * 1.25).round, :center
           end
 
           pdf.add_subtitle data[:name], PDF_FONT_SIZE, PDF_FONT_SIZE
 
           data[:column_data].each do |row|
-            new_row = {}
+            new_row = []
 
             row.each do |column_name, column_content|
-              new_row[column_name] = column_content.kind_of?(Array) ?
-                column_content.join("\n\n").to_iso :
-                column_content.to_iso
+              new_row << column_content.kind_of?(Array) ?
+                column_content.join("\n\n") :
+                column_content
             end
 
             column_data << new_row
           end
 
           unless column_data.blank?
-            PDF::SimpleTable.new do |table|
-              table.width = pdf.page_usable_width
-              table.columns = columns
-              table.data = column_data.sort do |row1, row2|
-                row1['score'].match(/(\d+)%/)[0].to_i <=>
-                  row2['score'].match(/(\d+)%/)[0].to_i
+            pdf.font_size((PDF_FONT_SIZE * 0.75).round) do
+              table_options = pdf.default_table_options(column_widths)
+
+              pdf.table(column_data.insert(0, column_headers), table_options) do
+                row(0).style(
+                  :background_color => 'cccccc',
+                  :padding => [(PDF_FONT_SIZE * 0.5).round, (PDF_FONT_SIZE * 0.3).round]
+                )
               end
-              table.column_order = @column_order
-              table.split_rows = true
-              table.row_gap = PDF_FONT_SIZE
-              table.font_size = (PDF_FONT_SIZE * 0.75).round
-              table.shade_color = Color::RGB.from_percentage(95, 95, 95)
-              table.shade_heading_color = Color::RGB.from_percentage(85, 85, 85)
-              table.heading_font_size = PDF_FONT_SIZE
-              table.shade_headings = true
-              table.position = :left
-              table.orientation = :right
-              table.render_on pdf
             end
           else
             pdf.text(
@@ -418,7 +386,7 @@ module ConclusionHighRiskReports
     end
 
     unless @filters.empty?
-      pdf.move_pointer PDF_FONT_SIZE
+      pdf.move_down PDF_FONT_SIZE
       pdf.text t('conclusion_committee_report.applied_filters',
         :filters => @filters.to_sentence, :count => @filters.size),
         :font_size => (PDF_FONT_SIZE * 0.75).round, :justification => :full
@@ -429,14 +397,14 @@ module ConclusionHighRiskReports
         :from_date => @from_date.to_formatted_s(:db),
         :to_date => @to_date.to_formatted_s(:db)), 'fixed_weaknesses_report', 0)
 
-    redirect_to PDF::Writer.relative_path(
+    redirect_to Prawn::Document.relative_path(
       t('conclusion_committee_report.fixed_weaknesses_report.pdf_name',
         :from_date => @from_date.to_formatted_s(:db),
         :to_date => @to_date.to_formatted_s(:db)), 'fixed_weaknesses_report', 0)
   end
-  
+
   private
-  
+
   def periods_by_solution_date_for_interval
     Period.includes(:reviews => [
         :conclusion_final_review, {:control_objective_items => :final_weaknesses}]
