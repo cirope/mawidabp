f36e1735485903be43d0f3f6d16a258124fc8143
Martín Vega <martin.vega7@gmail.com>
2013-05-14 15:08:29 -0300

    No conformidades potenciales. Refs #26

---
+++ app/controllers/potential_nonconformities_controller.rb
@@ -0,0 +1,316 @@
+# -*- coding: utf-8 -*-
+class PotentialNonconformitiesController < ApplicationController
+  before_filter :auth, :load_privileges, :check_privileges
+  hide_action :find_with_organization, :load_privileges
+  layout proc{ |controller| controller.request.xhr? ? false : 'application' }
+
+  # Lista las no conformidades potenciales
+  #
+  # * GET /potential_nonconformities
+  # * GET /potential_nonconformities.xml
+  def index
+    @title = t 'potential_nonconformity.index_title'
+    default_conditions = [
+      "#{Period.table_name}.organization_id = :organization_id",
+      [
+        [
+          "#{ConclusionReview.table_name}.review_id IS NULL",
+          "#{PotentialNonconformity.table_name}.final = :boolean_false"
+        ].join(' AND '),
+        [
+          "#{ConclusionReview.table_name}.review_id IS NOT NULL",
+          "#{PotentialNonconformity.table_name}.final = :boolean_true"
+        ].join(' AND ')
+      ].map {|condition| "(#{condition})"}.join(' OR ')
+    ]
+    parameters = {:organization_id => @auth_organization.id,
+      :boolean_true => true, :boolean_false => false}
+
+    if params[:control_objective].to_i > 0
+      default_conditions << "#{Weakness.table_name}.control_objective_item_id = " +
+        ":control_objective_id"
+      parameters[:control_objective_id] = params[:control_objective].to_i
+    end
+
+    if params[:review].to_i > 0
+      default_conditions << "#{Review.table_name}.id = :review_id"
+      parameters[:review_id] = params[:review].to_i
+    end
+
+    build_search_conditions PotentialNonconformity,
+      default_conditions.map { |c| "(#{c})" }.join(' AND ')
+
+    @potential_nonconformities = PotentialNonconformity.includes(
+      :work_papers,
+      :control_objective_item => {
+        :review => [:period, :plan_item, :conclusion_final_review]
+      }
+    ).where([@conditions, parameters]).order(
+      @order_by || [
+        "#{Review.table_name}.identification DESC",
+        "#{PotentialNonconformity.table_name}.review_code ASC"
+      ]
+    ).paginate(:page => params[:page], :per_page => APP_LINES_PER_PAGE)
+
+    respond_to do |format|
+      format.html {
+        if @potential_nonconformities.size == 1 && !@query.blank? && !params[:page]
+          redirect_to potential_nonconformity_url(@potential_nonconformities.first)
+        end
+      } # index.html.erb
+      format.xml  { render :xml => @potential_nonconformities }
+    end
+  end
+
+  # Muestra el detalle de una no conformidad potencial
+  #
+  # * GET /potential_nonconformities/1
+  # * GET /potential_nonconformities/1.xml
+  def show
+    @title = t 'potential_nonconformity.show_title'
+    @potential_nonconformity = find_with_organization(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @potential_nonconformity }
+    end
+  end
+
+  # Permite ingresar los datos para crear una nueva no conformidad potencial
+  #
+  # * GET /potential_nonconformities/new
+  # * GET /potential_nonconformities/new.xml
+  def new
+    @title = t 'potential_nonconformity.new_title'
+    @potential_nonconformity = PotentialNonconformity.new(
+      {:control_objective_item_id => params[:control_objective_item]}, {}, true
+    )
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @potential_nonconformity }
+    end
+  end
+
+  # Recupera los datos para modificar una no conformidad potencial
+  #
+  # * GET /potential_nonconformities/1/edit
+  def edit
+    @title = t 'potential_nonconformity.edit_title'
+    @potential_nonconformity = find_with_organization(params[:id])
+  end
+
+  # Crea una no conformidad potencial siempre que cumpla con las validaciones.
+  #
+  # * POST /potential_nonconformities
+  # * POST /potential_nonconformities.xml
+  def create
+    @title = t 'potential_nonconformity.new_title'
+    @potential_nonconformity = PotentialNonconformity.new(params[:potential_nonconformity])
+
+    respond_to do |format|
+      if @potential_nonconformity.save
+        flash.notice = t 'potential_nonconformity.correctly_created'
+        format.html { redirect_to(edit_potential_nonconformity_url(@potential_nonconformity)) }
+        format.xml  { render :xml => @potential_nonconformity, :status => :created, :location => @potential_nonconformity }
+      else
+        format.html { render :action => :new }
+        format.xml  { render :xml => @potential_nonconformity.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # Actualiza el contenido de una no conformidad potencial siempre que cumpla con
+  # las validaciones.
+  #
+  # * PUT /potential_nonconformities/1
+  # * PUT /potential_nonconformities/1.xml
+  def update
+    @title = t 'potential_nonconformity.edit_title'
+    @potential_nonconformity = find_with_organization(params[:id])
+
+    respond_to do |format|
+      PotentialNonconformity.transaction do
+        if @potential_nonconformity.update_attributes(params[:potential_nonconformity])
+          flash.notice = t 'potential_nonconformity.correctly_updated'
+          format.html { redirect_to(edit_potential_nonconformity_url(@potential_nonconformity)) }
+          format.xml  { head :ok }
+        else
+          format.html { render :action => :edit }
+          format.xml  { render :xml => @potential_nonconformity.errors, :status => :unprocessable_entity }
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+
+  rescue ActiveRecord::StaleObjectError
+    flash.alert = t 'potential_nonconformity.stale_object_error'
+    redirect_to :action => :edit
+  end
+
+  # Crea el documento de seguimiento de la oportunidad
+  #
+  # * GET /potential_nonconformities/follow_up_pdf/1
+  def follow_up_pdf
+    potential_nonconformity = find_with_organization(params[:id])
+
+    potential_nonconformity.follow_up_pdf(@auth_organization)
+
+    redirect_to potential_nonconformity.relative_follow_up_pdf_path
+  end
+
+  # Deshace la reiteración de la oportunidad
+  #
+  # * PUT /potential_nonconformities/undo_reiteration/1
+  def undo_reiteration
+    @potential_nonconformity = find_with_organization(params[:id])
+    @potential_nonconformity.undo_reiteration
+
+    respond_to do |format|
+      format.html { redirect_to(edit_potential_nonconformity_url(@potential_nonconformity)) }
+      format.xml  { head :ok }
+    end
+  end
+
+  # * POST /potential_nonconformities/auto_complete_for_user
+  def auto_complete_for_user
+    @tokens = params[:q][0..100].split(/[\s,]/).uniq
+    @tokens.reject! {|t| t.blank?}
+    conditions = [
+      "#{Organization.table_name}.id = :organization_id",
+      "#{User.table_name}.hidden = false"
+    ]
+    parameters = {:organization_id => @auth_organization.id}
+    @tokens.each_with_index do |t, i|
+      conditions << [
+        "LOWER(#{User.table_name}.name) LIKE :user_data_#{i}",
+        "LOWER(#{User.table_name}.last_name) LIKE :user_data_#{i}",
+        "LOWER(#{User.table_name}.function) LIKE :user_data_#{i}",
+        "LOWER(#{User.table_name}.user) LIKE :user_data_#{i}"
+      ].join(' OR ')
+
+      parameters[:"user_data_#{i}"] = "%#{Unicode::downcase(t)}%"
+    end
+
+    @users = User.includes(:organizations).where(
+      [conditions.map {|c| "(#{c})"}.join(' AND '), parameters]
+    ).order(
+      [
+        "#{User.table_name}.last_name ASC",
+        "#{User.table_name}.name ASC"
+      ]
+    ).limit(10)
+
+    respond_to do |format|
+      format.json { render :json => @users }
+    end
+  end
+
+  # * POST /potential_nonconformities/auto_complete_for_finding_relation
+  def auto_complete_for_finding_relation
+    @tokens = params[:q][0..100].split(SPLIT_AND_TERMS_REGEXP).uniq.map(&:strip)
+    @tokens.reject! { |t| t.blank? }
+    conditions = [
+      ("#{Finding.table_name}.id <> :finding_id" unless params[:finding_id].blank?),
+      "#{Finding.table_name}.final = :boolean_false",
+      "#{Period.table_name}.organization_id = :organization_id",
+      [
+        "#{ConclusionReview.table_name}.review_id IS NOT NULL",
+        ("#{Review.table_name}.id = :review_id" unless params[:review_id].blank?)
+      ].compact.join(' OR ')
+    ].compact
+    parameters = {
+      :boolean_false => false,
+      :finding_id => params[:finding_id],
+      :organization_id => @auth_organization.id,
+      :review_id => params[:review_id]
+    }
+    @tokens.each_with_index do |t, i|
+      conditions << [
+        "LOWER(#{Finding.table_name}.review_code) LIKE :finding_relation_data_#{i}",
+        "LOWER(#{Finding.table_name}.description) LIKE :finding_relation_data_#{i}",
+        "LOWER(#{ControlObjectiveItem.table_name}.control_objective_text) LIKE :finding_relation_data_#{i}",
+        "LOWER(#{Review.table_name}.identification) LIKE :finding_relation_data_#{i}",
+      ].join(' OR ')
+
+      parameters[:"finding_relation_data_#{i}"] = "%#{Unicode::downcase(t)}%"
+    end
+
+    @findings = Finding.includes(
+      :control_objective_item => {
+        :review => [:period, :conclusion_final_review]
+      }
+    ).where([conditions.map {|c| "(#{c})"}.join(' AND '), parameters]).order(
+      [
+        "#{Review.table_name}.identification ASC",
+        "#{Finding.table_name}.review_code ASC"
+      ]
+    ).limit(5)
+
+    respond_to do |format|
+      format.json { render :json => @findings }
+    end
+  end
+
+  # * POST /potential_nonconformities/auto_complete_for_control_objective_item
+  def auto_complete_for_control_objective_item
+    @tokens = params[:q][0..100].split(SEARCH_AND_REGEXP).uniq
+    @tokens.reject! {|t| t.blank?}
+    conditions = [
+      "#{Period.table_name}.organization_id = :organization_id",
+      "#{ConclusionReview.table_name}.review_id IS NULL",
+      "#{ControlObjectiveItem.table_name}.review_id = :review_id"
+    ]
+    parameters = {
+      :organization_id => @auth_organization.id,
+      :review_id => params[:review_id].to_i
+    }
+
+    @tokens.each_with_index do |t, i|
+      conditions << [
+        "LOWER(#{ControlObjectiveItem.table_name}.control_objective_text) LIKE :control_objective_item_data_#{i}"
+      ].join(' OR ')
+
+      parameters[:"control_objective_item_data_#{i}"] =
+        "%#{Unicode::downcase(t)}%"
+    end
+
+    @control_objective_items = ControlObjectiveItem.includes(
+      :review => [:period, :conclusion_final_review]
+    ).where(
+      conditions.map {|c| "(#{c})"}.join(' AND '), parameters
+    ).order("#{Review.table_name}.identification ASC").limit(10)
+
+    respond_to do |format|
+      format.json { render :json => @control_objective_items }
+    end
+  end
+
+  private
+
+  # Busca la no conformidad potencial indicada siempre que pertenezca a la
+  # organización. En el caso que no se encuentre (ya sea que no existe una
+  # oportunidad con ese ID o que no pertenece a la organización con la que se
+  # autenticó el usuario) devuelve nil.
+  # _id_::  ID de la oportunidad que se quiere recuperar
+  def find_with_organization(id) #:doc:
+    PotentialNonconformity.includes(
+      :finding_relations,
+      :work_papers,
+      {:finding_user_assignments => :user},
+      {:control_objective_item => {:review => :period}}
+    ).where(
+      :id => id, Period.table_name => {:organization_id => @auth_organization.id}
+    ).first(:readonly => false)
+  end
+
+  def load_privileges #:nodoc:
+    @action_privileges.update(
+      :follow_up_pdf => :read,
+      :auto_complete_for_user => :read,
+      :auto_complete_for_finding_relation => :read,
+      :auto_complete_for_control_objective_item => :read,
+      :undo_reiteration => :modify
+    )
+  end
+end
---
+++ app/models/potential_nonconformity.rb
@@ -0,0 +1,111 @@
+class PotentialNonconformity < Finding
+# Acceso a los atributos
+  attr_reader :approval_errors
+
+  # Named scopes
+  scope :all_for_report, lambda {
+    where(
+      :state => STATUS.except(*EXCLUDE_FROM_REPORTS_STATUS).values,
+      :final => true
+    ).order('state ASC')
+  }
+
+  # Restricciones
+  validates_each :review_code do |record, attr, value|
+    prefix = record.get_parameter(:admin_code_prefix_for_potential_nonconformities, false,
+      record.control_objective_item.try(:review).try(:organization).try(:id))
+    regex = /\A#{prefix}\d+\Z/
+
+    record.errors.add attr, :invalid unless value =~ regex
+  end
+
+  def initialize(attributes = nil, options = {}, import_users = false)
+    super(attributes, options, import_users)
+
+    self.review_code ||= self.next_code
+  end
+
+  def self.columns_for_sort
+    Finding.columns_for_sort.except(
+      :risk_asc, :risk_desc, :follow_up_date_asc, :follow_up_date_desc
+    )
+  end
+
+  def prepare_work_paper(work_paper)
+    work_paper.code_prefix = self.get_parameter(
+      :admin_code_prefix_for_work_papers_in_potential_nonconformities)
+  end
+
+  def prefix
+    self.control_objective_item.try(:review) ?
+      self.get_parameter(:admin_code_prefix_for_potential_nonconformities, false,
+      self.control_objective_item.review.organization.id) : nil
+  end
+
+  def next_code(review = nil)
+    review ||= self.control_objective_item.try(:reload).try(:review)
+    code_prefix = self.parameter_in(GlobalModelConfig.current_organization_id,
+      :admin_code_prefix_for_potential_nonconformities, review.try(:created_at))
+
+    review ? review.next_potential_nonconformity_code(code_prefix) : "#{code_prefix}1".strip
+  end
+
+  def last_work_paper_code(review = nil)
+    review ||= self.control_objective_item.try(:reload).try(:review)
+    code_prefix = self.parameter_in(GlobalModelConfig.current_organization_id,
+      :admin_code_prefix_for_work_papers_in_potential_nonconformities,
+      review.try(:created_at))
+
+    code_from_review = review ?
+      review.last_potential_nonconformity_work_paper_code(code_prefix) :
+      "#{code_prefix} 0".strip
+
+    code_from_potential_nonconformity = self.work_papers.reject(
+      &:marked_for_destruction?).map(
+      &:code).select { |c| c =~ /#{code_prefix}\s\d+/ }.sort.last
+
+    [code_from_review, code_from_potential_nonconformity].compact.max
+  end
+
+  def must_be_approved?
+    return true if self.revoked?
+
+    errors = []
+
+    if self.implemented_audited? && self.solution_date.blank?
+      errors << I18n.t('potential_nonconformity.errors.without_solution_date')
+    elsif self.implemented?
+      if self.solution_date?
+        errors << I18n.t('potential_nonconformity.errors.with_solution_date')
+      end
+    elsif self.being_implemented?
+      if self.answer.blank?
+        errors << I18n.t('potential_nonconformity.errors.without_answer')
+      end
+
+      if self.solution_date?
+        errors << I18n.t('potential_nonconformity.errors.with_solution_date')
+      end
+    elsif self.assumed_risk? && self.follow_up_date?
+      errors << I18n.t('potential_nonconformity.errors.with_follow_up_date')
+    elsif !self.implemented_audited? && !self.implemented? &&
+        !self.being_implemented? && !self.unconfirmed? &&
+        !self.assumed_risk?
+      errors << I18n.t('potential_nonconformity.errors.not_valid_state')
+    end
+
+    unless self.has_audited?
+      errors << I18n.t('potential_nonconformity.errors.without_audited')
+    end
+
+    unless self.has_auditor?
+      errors << I18n.t('potential_nonconformity.errors.without_auditor')
+    end
+
+    if self.audit_comments.blank? && !self.revoked?
+      errors << I18n.t('potential_nonconformity.errors.without_audit_comments')
+    end
+
+    (@approval_errors = errors).blank?
+  end
+end
