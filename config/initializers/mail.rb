4b9e0640248c4853112d87503436a5b790a63bf1
Franco Catena <francocatena@gmail.com>
2013-09-23 15:05:18 -0300

    Remove email method in config

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,18 +1,18 @@
-MawidaBP::Application.config.action_mailer.default_url_options = {
-  :host => URL_HOST
-}
-MawidaBP::Application.config.action_mailer.raise_delivery_errors =
-  !Rails.env.production?
-MawidaBP::Application.config.action_mailer.delivery_method = :smtp
-MawidaBP::Application.config.action_mailer.smtp_settings = {
-  :address => 'smtp.gmail.com',
-  :domain => 'mawidabp.com',
-  :port => 587,
-  :user_name => 'soporte@mawidabp.com',
-  :password => APP_CONFIG['smtp_password'],
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+MawidaBP::Application.configure do
+  config.action_mailer.default_url_options = {
+    :host => URL_HOST
+  }
+  config.action_mailer.raise_delivery_errors = !Rails.env.production?
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :domain => 'mawidabp.com',
+    :port => 587,
+    :user_name => 'soporte@mawidabp.com',
+    :password => APP_CONFIG['smtp_password'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
 
 ActionMailer::Base.register_observer(MailObserver)
 
