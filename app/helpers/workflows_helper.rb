8a7339eeb231e8771592615f529e08f80980fd00
Franco Catena <francocatena@gmail.com>
2013-09-25 14:55:30 -0300

    WorkflowsController warnings fix #43

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -2,10 +2,10 @@ module WorkflowsHelper
   def show_detailed_distance_of_time(start_date, end_date)
     distance_text = extended_distance_of_time_in_words(start_date, end_date)
     interval = WorkflowItem.human_attribute_name(:start) + ': ' +
-      l(start_date, :format => :long) + ', ' +
+      l(start_date, format: :long) + ', ' +
       WorkflowItem.human_attribute_name(:end) + ': ' +
-      l(end_date, :format => :long)
+      l(end_date, format: :long)
 
-    content_tag(:abbr, distance_text, :title => interval)
+    content_tag(:abbr, distance_text, title: interval)
   end
 end
