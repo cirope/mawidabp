ed6441d4dc81e5f45a53d8bedceabba223fc52fd
Franco Catena <francocatena@gmail.com>
2013-09-25 11:42:16 -0300

    ErrorRecordsController warnings fix #43

--- app/controllers/error_records_controller.rb
+++ app/controllers/error_records_controller.rb
@@ -14,7 +14,7 @@ class ErrorRecordsController < ApplicationController
     @from_date, @to_date = *make_date_range(params[:index])
     default_conditions = [
       "#{ErrorRecord.table_name}.organization_id = :organization_id",
-      {:organization_id => @auth_organization.id}
+      { organization_id: @auth_organization.id }
     ]
 
     unless params[:search]
@@ -23,8 +23,8 @@ class ErrorRecordsController < ApplicationController
         "#{ErrorRecord.table_name}.created_at BETWEEN :from_date AND :to_date"
       ].join(' AND ')
 
-      default_conditions[1].merge!(:from_date => @from_date,
-        :to_date => @to_date.to_time.end_of_day)
+      default_conditions[1].merge!(from_date: @from_date,
+        to_date: @to_date.to_time.end_of_day)
     else
       build_search_conditions ErrorRecord, default_conditions
     end
@@ -32,8 +32,8 @@ class ErrorRecordsController < ApplicationController
     @error_records = ErrorRecord.includes(:user).where(
       @conditions || default_conditions
     ).order("#{ErrorRecord.table_name}.created_at DESC").paginate(
-      :page => params[:page], :per_page => APP_LINES_PER_PAGE
-    )
+      page: params[:page], per_page: APP_LINES_PER_PAGE
+    ).references(:users)
 
     respond_to do |format|
       format.html {
@@ -41,7 +41,7 @@ class ErrorRecordsController < ApplicationController
           redirect_to error_record_path(@error_records.first)
         end
       } # index.html.erb
-      format.xml  { render :xml => @error_records }
+      format.xml  { render xml: @error_records }
     end
   end
 
@@ -52,12 +52,12 @@ class ErrorRecordsController < ApplicationController
   def show
     @title = t 'error_record.show_title'
     @error_record = ErrorRecord.where(
-      :organization_id => @auth_organization.id, :id => params[:id]
+      organization_id: @auth_organization.id, id: params[:id]
     ).first
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @error_record }
+      format.xml  { render xml: @error_record }
     end
   end
 
@@ -73,9 +73,9 @@ class ErrorRecordsController < ApplicationController
           'created_at BETWEEN :from_date AND :to_date'
         ].join(' AND '),
         {
-          :from_date => from_date,
-          :to_date => to_date.to_time.end_of_day,
-          :organization_id => @auth_organization.id
+          from_date: from_date,
+          to_date: to_date.to_time.end_of_day,
+          organization_id: @auth_organization.id
         }
       ]
     ).order('created_at DESC')
@@ -89,8 +89,8 @@ class ErrorRecordsController < ApplicationController
 
     pdf.add_description_item(t('error_record.period.title'),
       t('error_record.period.range',
-        :from_date => l(from_date, :format => :long),
-        :to_date => l(to_date, :format => :long)))
+        from_date: l(from_date, format: :long),
+        to_date: l(to_date, format: :long)))
 
     column_order = [['user_id', 20], ['created_at', 15], ['error', 15],
       ['data', 50]]
@@ -107,7 +107,7 @@ class ErrorRecordsController < ApplicationController
       column_data << [
         "<b>#{user_name}</b>",
         error_record.created_at ?
-          l(error_record.created_at, :format => :minimal) : '-',
+          l(error_record.created_at, format: :minimal) : '-',
         error_record.error_text,
         error_record.data
       ]
@@ -121,16 +121,16 @@ class ErrorRecordsController < ApplicationController
 
         pdf.table(column_data.insert(0, column_headers), table_options) do
           row(0).style(
-            :background_color => 'cccccc',
-            :padding => [(PDF_FONT_SIZE * 0.5).round, (PDF_FONT_SIZE * 0.3).round]
+            background_color: 'cccccc',
+            padding: [(PDF_FONT_SIZE * 0.5).round, (PDF_FONT_SIZE * 0.3).round]
           )
         end
       end
     end
 
     pdf_name = t('error_record.pdf_list_name',
-      :from_date => from_date.to_formatted_s(:db),
-      :to_date => to_date.to_formatted_s(:db))
+      from_date: from_date.to_formatted_s(:db),
+      to_date: to_date.to_formatted_s(:db))
 
     pdf.custom_save_as(pdf_name, ErrorRecord.table_name)
 
@@ -140,8 +140,6 @@ class ErrorRecordsController < ApplicationController
   private
 
   def load_privileges #:nodoc:
-    @action_privileges.update({
-        :export_to_pdf => :read
-      })
+    @action_privileges.update(export_to_pdf: :read)
   end
 end
