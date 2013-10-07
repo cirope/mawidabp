76e178cbbed9254d5406dd6728b717fafe65cf19
Franco Catena <francocatena@gmail.com>
2013-10-06 23:28:24 -0300

    New schedule config

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,9 +5,7 @@
 # Para eliminarla
 # whenever -c mawidabp
 
-env :PATH, '"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
-
-every 1.day, :at => '20:00' do
+every 1.day, at: '20:00' do
   runner 'ConclusionFinalReview.warning_auditors_about_close_date'
   runner 'Finding.notify_for_unconfirmed_for_notification_findings'
   runner 'User.notify_new_findings'
