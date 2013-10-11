0e2dd132ad8d2fa6e253b29be9984af079b33d26
Ismael Isuani <ismaelisuani@gmail.com>
2013-10-11 10:28:05 -0300

    Show value in select parameter

--- app/helpers/parameters_helper.rb
+++ app/helpers/parameters_helper.rb
@@ -6,21 +6,25 @@ module ParametersHelper
 
   def relevance_field(form, *options)
     form.select :relevance,
-      RELEVANCE_TYPES.map { |k,v| [t("relevance_types.#{k}"), v] }, *options
+      RELEVANCE_TYPES.map { |k,v|
+        [[t("relevance_types.#{k}"),"(#{v})"].join(' '), v] }, *options
   end
 
   def qualification_field(form, field, *options)
     form.select field,
-      QUALIFICATION_TYPES.map { |k,v| [t("qualification_types.#{k}"), v] }, *options
+      QUALIFICATION_TYPES.map { |k,v|
+        [[t("qualification_types.#{k}"), "(#{v})"].join(' '), v] }, *options
   end
 
   def risk_field(form, *options)
     form.select :risk,
-      RISK_TYPES.map { |k,v| [t("risk_types.#{k}"), v] }, *options
+      RISK_TYPES.map { |k,v|
+        [[t("risk_types.#{k}"), "(#{v})"].join(' '), v] }, *options
   end
 
   def priority_field(form, *options)
     form.select :priority,
-      PRIORITY_TYPES.map { |k,v| [t("priority_types.#{k}"), v] }, *options
+      PRIORITY_TYPES.map { |k,v|
+        [[t("priority_types.#{k}"), "(#{v})"].join(' '), v] }, *options
   end
 end
