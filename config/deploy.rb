93536e68ad0fba3f96e59b48fe22c7e206f63bd9
Franco Catena <francocatena@gmail.com>
2013-11-10 14:23:41 -0300

    Remove unicorn start in deploy restart =)

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,6 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute 'service unicorn start'
       execute 'service unicorn upgrade'
     end
   end
