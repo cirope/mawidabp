fcebc0e37f78a08a8ecc3574434ed52ca58380df
Ismael Isuani <ismaelisuani@gmail.com>
2013-11-21 12:15:12 -0300

    cleanup finding scopes

--- app/models/finding.rb
+++ app/models/finding.rb
@@ -159,12 +159,7 @@ class Finding < ActiveRecord::Base
   scope :for_notification, -> { where(:state => STATUS[:notify], :final => false) }
   scope :finals, ->(use_finals) { where(:final => use_finals) }
   scope :sort_by_code, -> { order('review_code ASC') }
-  scope :for_current_organization, -> {
-    includes(:control_objective_item => {:review => :period}).where(
-      "#{Period.table_name}.organization_id = :organization_id",
-      :organization_id => Organization.current_id
-    ).references(:periods)
-  }
+  scope :for_current_organization, -> { list }
   scope :for_period, ->(period) {
     includes(:control_objective_item => { :review =>:period }).where(
       "#{Period.table_name}.id" => period.id
@@ -235,31 +230,31 @@ class Finding < ActiveRecord::Base
     ).references(:periods)
   }
   scope :unconfirmed_and_stale, -> {
-  stale_parameters = Organization.all_parameters('finding_stale_confirmed_days')
-  pre_conditions = []
-  parameters = {
-    :state => STATUS[:unconfirmed],
-    :boolean_false => false
-  }
+    stale_parameters = Organization.all_parameters('finding_stale_confirmed_days')
+    pre_conditions = []
+    parameters = {
+      :state => STATUS[:unconfirmed],
+      :boolean_false => false
+    }
 
-  stale_parameters.each_with_index do |stale_parameter, i|
-    stale_days = stale_parameter[:parameter].to_i
-    parameters[:"stale_unconfirmed_date_#{i}"] =
-      (FINDING_STALE_UNCONFIRMED_DAYS + stale_days).days.ago_in_business.to_date
-    parameters[:"organization_id_#{i}"] = stale_parameter[:organization].id
+    stale_parameters.each_with_index do |stale_parameter, i|
+      stale_days = stale_parameter[:parameter].to_i
+      parameters[:"stale_unconfirmed_date_#{i}"] =
+        (FINDING_STALE_UNCONFIRMED_DAYS + stale_days).days.ago_in_business.to_date
+      parameters[:"organization_id_#{i}"] = stale_parameter[:organization].id
 
-    pre_conditions << [
-      "first_notification_date < :stale_unconfirmed_date_#{i}",
-      "#{Period.table_name}.organization_id = :organization_id_#{i}",
+      pre_conditions << [
+        "first_notification_date < :stale_unconfirmed_date_#{i}",
+        "#{Period.table_name}.organization_id = :organization_id_#{i}",
+      ].join(' AND ')
+    end
+
+    fix_conditions = [
+      'state = :state',
+      'final = :boolean_false'
     ].join(' AND ')
-  end
 
-  fix_conditions = [
-    'state = :state',
-    'final = :boolean_false'
-  ].join(' AND ')
-
-  includes(:control_objective_item => { :review => :period }).where(
+    includes(:control_objective_item => { :review => :period }).where(
       [
         "(#{pre_conditions.map { |c| "(#{c})" }.join(' OR ')})", fix_conditions
       ].join(' AND '),
@@ -310,41 +305,28 @@ class Finding < ActiveRecord::Base
   scope :being_implemented, -> { where(:state => STATUS[:being_implemented]) }
   scope :not_incomplete, -> { where("state <> ?", Finding::STATUS[:incomplete]) }
   scope :list_all_by_date, ->(from_date, to_date, order) {
-    includes(
-      :control_objective_item => {
-        :review => [:period, :conclusion_final_review, {:plan_item => :business_unit}]
-      }
+    list.includes(
+      review: [:period, :conclusion_final_review, {:plan_item => :business_unit}]
     ).where(
-      [
-        "#{ConclusionReview.table_name}.issue_date BETWEEN :begin AND :end",
-        "#{Period.table_name}.organization_id = :organization_id"
-      ].join(' AND '),
-      {
-        :begin => from_date, :end => to_date,
-        :organization_id => Organization.current_id
-      }
+      "#{ConclusionReview.table_name}.issue_date BETWEEN :begin AND :end",
+      { :begin => from_date, :end => to_date }
     ).references(:conslusion_reviews, :periods).order(
       order ?
         ["#{Period.table_name}.start ASC", "#{Period.table_name}.end ASC"] : nil
     )
   }
-  scope :with_status_for_report, -> { where(
-    :state => STATUS.except(*EXCLUDE_FROM_REPORTS_STATUS).values
-    )
+  scope :with_status_for_report, -> {
+    where(:state => STATUS.except(*EXCLUDE_FROM_REPORTS_STATUS).values)
   }
   scope :list_all_in_execution_by_date, ->(from_date, to_date) {
-    includes(
+    list.includes(
       :control_objective_item => {:review => [:period, :conclusion_final_review]}
     ).where(
       [
         "#{Review.table_name}.created_at BETWEEN :begin AND :end",
-        "#{Period.table_name}.organization_id = :organization_id",
         "#{ConclusionFinalReview.table_name}.review_id IS NULL"
       ].join(' AND '),
-      {
-        :begin => from_date, :end => to_date,
-        :organization_id => Organization.current_id
-      }
+      { :begin => from_date, :end => to_date }
     ).references(:reviews, :periods, :conclusion_reviews)
   }
   scope :internal_audit, -> {
