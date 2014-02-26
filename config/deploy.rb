5211ac1acffeb4f0425c6c67641fa1b027100a69
Franco Catena <francocatena@gmail.com>
2014-02-26 00:21:08 -0300

    Ruby 2.1.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :linked_files, %w{config/app_config.yml}
 set :linked_dirs, %w{log private public/error_files}
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.0'
+set :rbenv_ruby, '2.1.1'
 
 set :keep_releases, 5
 
