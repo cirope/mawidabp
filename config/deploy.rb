c826b2967de36743c175bffe7437fa7c57b6ce2e
Franco Catena <francocatena@gmail.com>
2014-05-03 15:29:29 -0300

    No more link to public/error_files in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :deploy_via, :remote_cache
 set :scm, :git
 
 set :linked_files, %w{config/application.yml}
-set :linked_dirs, %w{log private public/error_files}
+set :linked_dirs, %w{log private}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.1.1'
