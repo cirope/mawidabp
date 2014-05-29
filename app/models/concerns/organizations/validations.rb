cc4c3c3e214c25ca7b460342a662cdd1aa998309
Franco Catena <francocatena@gmail.com>
2014-05-29 16:03:33 -0300

    Organize organization validations =)

--- app/models/concerns/organizations/validations.rb
+++ app/models/concerns/organizations/validations.rb
@@ -2,15 +2,12 @@ module Organizations::Validations
   extend ActiveSupport::Concern
 
   included do
-    validates :prefix, format: { with: /\A[A-Za-z][A-Za-z0-9\-]+\z/ },
-      allow_nil: true, allow_blank: true
-    validates :name, :prefix, :kind, presence: true
-    validates :name, :prefix, length: { maximum: 255 }, allow_nil: true,
-      allow_blank: true
-    validates :prefix, uniqueness: { case_sensitive: false }
+    validates :name, :prefix, presence: true, length: { maximum: 255 }
     validates :name, uniqueness: { case_sensitive: false, scope: :group_id }
-    validates :prefix, exclusion: { in: APP_ADMIN_PREFIXES }
-    validates :kind, inclusion: { in: ORGANIZATION_KINDS }, allow_nil: true,
-      allow_blank: true
+    validates :kind, presence: true, inclusion: { in: ORGANIZATION_KINDS }
+    validates :prefix,
+      format: { with: /\A[A-Za-z][A-Za-z0-9\-]+\z/ },
+      uniqueness: { case_sensitive: false },
+      exclusion: { in: APP_ADMIN_PREFIXES }
   end
 end
