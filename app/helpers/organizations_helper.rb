9280e88a99becea9fd0d5ea1820f54c89c07e88f
Franco Catena <francocatena@gmail.com>
2014-05-29 19:22:00 -0300

    Organization's views cleanup

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,21 +1,32 @@
 module OrganizationsHelper
-  def organization_image_tag(thumb_name = :thumb)
-    real_id = Organization.current_id
-    Organization.current_id = @organization.id
-
-    out = image_tag @organization.image_model.image.url(thumb_name),
-      :size => @organization.image_model.image_size(thumb_name)
-
-    Organization.current_id = real_id
-
-    out
+  def organization_image_tag thumb_name = :thumb
+    scoped_organization_image thumb_name if image_persisted?
   end
 
-  def kind_field(form)
-    collection = ORGANIZATION_KINDS.collect do |kind|
+  def organization_image
+    @organization.build_image_model unless @organization.image_model
+
+    @organization.image_model
+  end
+
+  def organization_kinds
+    collection = ORGANIZATION_KINDS.map do |kind|
       [t("activerecord.attributes.organization.kind_options.#{kind}"), kind]
     end
-
-    form.input :kind, collection: collection, prompt: true
   end
+
+  private
+
+    def image_persisted?
+      @organization.image_model && !@organization.image_model.image_cache
+    end
+
+    def scoped_organization_image thumb_name
+      Fiber.new do
+        Organization.current_id = @organization.id
+
+        image_tag @organization.image_model.image.url(thumb_name),
+          size: @organization.image_model.image_size(thumb_name)
+      end.resume
+    end
 end
