d4dbe2691faa44ced80385f8c8c123b778342476
Ismael Isuani <ismaelisuani@gmail.com>
2014-05-29 21:19:20 -0300

    Improve refactor

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,6 +5,7 @@ class SessionsControllerTest < ActionController::TestCase
 
   def setup
     @organization = organizations(:cirope)
+    @user = users :administrator_user
 
     @request.host = "#{@organization.prefix}.localhost.i"
   end
@@ -15,69 +16,25 @@ class SessionsControllerTest < ActionController::TestCase
     assert_template 'sessions/new'
   end
 
-  test 'invalid password attempt in admin mode' do
-    @request.host = "#{APP_ADMIN_PREFIXES.first}.localhost.i"
+  test 'login without organization' do
+    @request.host = 'localhost.i'
+    post :create, user: @user.user, password: ::PLAIN_PASSWORDS[@user.user]
 
-    assert_difference 'ErrorRecord.count' do
-      post :create, :user => users(:administrator_user).user,
-        :password => 'wrong password'
-
-      error_record = ErrorRecord.where(
-        :user_id => users(:administrator_user).id,
-        :error => ErrorRecord::ERRORS[:on_login]
-      ).order('created_at DESC').first
-      assert_kind_of ErrorRecord, error_record
-      assert_redirected_to login_url
-      assert_equal I18n.t('message.invalid_user_or_password'), flash.alert
-    end
-  end
-
-  test 'disabled user attempt in admin mode' do
-    @request.host = "#{APP_ADMIN_PREFIXES.first}.localhost.i"
-
-    assert_difference 'ErrorRecord.count' do
-      post :create, :user => users(:disabled_user).user,
-        :password => ::PLAIN_PASSWORDS[users(:disabled_user).user]
-
-      error_record = ErrorRecord.where(
-        :user_id => users(:disabled_user).id,
-        :error => ErrorRecord::ERRORS[:on_login]
-      ).order('created_at DESC').first
-      assert_kind_of ErrorRecord, error_record
-      assert_redirected_to login_url
-      assert_equal I18n.t('message.invalid_user_or_password'), flash.alert
-    end
-  end
-
-  test 'no group admin user attempt in admin mode' do
-    @request.host = "#{APP_ADMIN_PREFIXES.first}.localhost.i"
-
-    assert_difference 'ErrorRecord.count' do
-      post :create, :user => users(:administrator_second_user).user,
-        :password => ::PLAIN_PASSWORDS[users(:administrator_second_user).user]
-
-      error_record = ErrorRecord.where(
-        :user_id => users(:administrator_second_user).id,
-        :error => ErrorRecord::ERRORS[:on_login]
-      ).order('created_at DESC').first
-      assert_kind_of ErrorRecord, error_record
-      assert_redirected_to login_url
-      assert_equal I18n.t('message.invalid_user_or_password'), flash.alert
-    end
+    assert_redirected_to login_url
+    assert_equal I18n.t('message.no_organization'), flash.alert
   end
 
   test 'excede maximun number off wrong attempts' do
-    user = User.find users(:administrator_user).id
-    max_attempts = user.get_parameter(
+    max_attempts = @user.get_parameter(
       :attempts_count, false, @organization.id
     ).to_i
 
     assert_difference 'ErrorRecord.count', max_attempts + 1 do
       max_attempts.times do
-        post :create, :user => user.user, :password => 'wrong password'
+        post :create, user: @user.user, password: 'wrong password'
 
         error_record = ErrorRecord.where(
-          :user_id => user.id, :error => ErrorRecord::ERRORS[:on_login]
+          user_id: @user.id, error: ErrorRecord::ERRORS[:on_login]
         ).order('created_at DESC').first
         assert_kind_of ErrorRecord, error_record
         assert_redirected_to login_url
@@ -86,60 +43,16 @@ class SessionsControllerTest < ActionController::TestCase
 
       assert_redirected_to login_url
       error_record = ErrorRecord.where(
-        :user_id => users(:administrator_user).id,
-        :error => ErrorRecord::ERRORS[:user_disabled]
+        user_id: @user.id, error: ErrorRecord::ERRORS[:user_disabled]
       ).order('created_at DESC').first
       assert_kind_of ErrorRecord, error_record
-      assert_equal max_attempts, user.reload.failed_attempts
-      assert !user.enable?
+      assert_equal max_attempts, @user.reload.failed_attempts
+      assert !@user.enable?
     end
   end
 
-  test 'login without organization' do
-    @request.host = 'localhost.i'
-
-    post :create, :user => users(:administrator_user).user,
-      :password => ::PLAIN_PASSWORDS[users(:administrator_user).user]
-
-    assert_redirected_to login_url
-    assert_equal I18n.t('message.no_organization'), flash.alert
-  end
-
-  test 'expired password' do
-    user = User.find users(:administrator_user).id
-    user.update_attribute :password_changed,
-      get_test_parameter(:password_expire_time).to_i.next.days.ago
-
-    post :create, :user => users(:administrator_user).user,
-      :password => ::PLAIN_PASSWORDS[users(:administrator_user).user]
-
-    assert_redirected_to edit_password_user_url(user)
-  end
-
-  test 'warning about password expiration' do
-    password_changed = get_test_parameter(
-      :expire_notification).to_i.next.days.ago
-    user = User.find users(:administrator_user).id
-
-    user.update_attribute :password_changed, password_changed
-
-    post :create, :user => users(:administrator_user).user,
-      :password => ::PLAIN_PASSWORDS[users(:administrator_user).user]
-
-    assert_redirected_to welcome_url
-    login_record = LoginRecord.where(
-      :user_id => users(:administrator_user).id,
-      :organization_id => organizations(:cirope).id
-    ).first
-    assert_kind_of LoginRecord, login_record
-    assert_not_nil I18n.t('message.password_expire_in_x',
-      :count => get_test_parameter(:expire_notification).to_i - 2),
-      flash.notice
-  end
-
   test 'redirected instead of relogin' do
-    post :create, :user => users(:administrator_user).user,
-      :password => ::PLAIN_PASSWORDS[users(:administrator_user).user]
+    post :create, user: @user.user, password: ::PLAIN_PASSWORDS[@user.user]
 
     assert_redirected_to welcome_url
     get :new
@@ -162,6 +75,7 @@ class SessionsControllerTest < ActionController::TestCase
 
   test 'logout' do
     login
+
     delete :destroy
     assert_nil session[:user_id]
     assert_redirected_to login_url
--- test/services/authentication_test.rb
+++ test/services/authentication_test.rb
@@ -10,21 +10,21 @@ class AuthenticationTest < ActionController::TestCase
     Organization.current_id = @organization.id
   end
 
-  test 'should authenticate admin mode' do
-    @auth = Authentication.new @params, request, session, @organization, true
-
-    assert_valid_authentication redirect_url: Group
-  end
-
-  test 'should authenticate normal mode' do
-    @auth = Authentication.new @params, request, session, @organization, false
-
+  test 'should authenticate' do
+    assert_valid_authentication redirect_url: Group, admin_mode: true
     assert_valid_authentication
   end
 
+  test 'no group admin user attempt login in admin mode' do
+    request.host = "#{APP_ADMIN_PREFIXES.first}.localhost.i"
+    @user.update_column :group_admin, false
+
+    assert_invalid_authentication admin_mode: true
+  end
+
   test 'should show message days for password expiration' do
-    @user.update_column :password_changed, 28.days.ago
-    @auth = Authentication.new @params, request, session, @organization, false
+    password_changed = get_test_parameter(:expire_notification).to_i.next.days.ago
+    @user.update_column :password_changed, password_changed
 
     days_for_password_expiration = @user.days_for_password_expiration
     message = I18n.t(days_for_password_expiration >= 0 ? 'message.password_expire_in_x' :
@@ -36,7 +36,6 @@ class AuthenticationTest < ActionController::TestCase
   test 'should show message must change the password' do
     @user.update_column :password_changed, get_test_parameter(
       :password_expire_time).to_i.next.days.ago
-    @auth = Authentication.new @params, request, session, @organization, false
 
     assert_valid_authentication redirect_url: ['edit_password', @user],
       message: I18n.t('message.must_change_the_password')
@@ -44,10 +43,9 @@ class AuthenticationTest < ActionController::TestCase
 
   test 'should show message pending poll' do
     user = users :poll_user
-    params = { user: user.user, password: ::PLAIN_PASSWORDS[user.user] }
+    @params = { user: user.user, password: ::PLAIN_PASSWORDS[user.user] }
     poll = user.first_pending_poll
     poll_redirect = ['edit', poll, token: poll.access_token, layout: 'clean']
-    @auth = Authentication.new params, request, session, @organization, false
 
     assert_valid_authentication redirect_url: poll_redirect,
       message: I18n.t('poll.must_answer_poll')
@@ -55,22 +53,14 @@ class AuthenticationTest < ActionController::TestCase
 
   test 'should not login with invalid password' do
     @params = { user: @user.user, password: 'wrong password' }
-    @auth = Authentication.new @params, request, session, @organization, false
 
     assert_invalid_authentication
-  end
-
-  test 'should not login with invalid password in admin mode' do
-    @params = { user: @user.user, password: 'wrong password' }
-    @auth = Authentication.new @params, request, session, @organization, true
-
-    assert_invalid_authentication
+    assert_invalid_authentication admin_mode: true
   end
 
   test 'should not login if user expired' do
     @user.update_column :last_access,
       get_test_parameter(:account_expire_time).to_i.days.ago.yesterday
-    @auth = Authentication.new @params, request, session, @organization, false
 
     assert_invalid_authentication
     assert !@user.reload.enable?
@@ -78,21 +68,19 @@ class AuthenticationTest < ActionController::TestCase
 
   test 'should not login user as disabled' do
     @user.update_column :enable, false
-    @auth = Authentication.new @params, request, session, @organization, false
 
     assert_invalid_authentication
+    assert_invalid_authentication admin_mode: true
   end
 
   test 'should not login user as hidden' do
     @user.update_column :hidden, true
-    @auth = Authentication.new @params, request, session, @organization, false
 
     assert_invalid_authentication
   end
 
   test 'should not login with hashed password' do
     @params = { user: @user.user, password: @user.password }
-    @auth = Authentication.new @params, request, session, @organization, false
 
     assert_invalid_authentication
   end
@@ -101,14 +89,15 @@ class AuthenticationTest < ActionController::TestCase
     setting = @organization.settings.find_by name: 'allow_concurrent_sessions'
     setting.update_column :value, 0
     @user.update! last_access: 1.minute.ago, logged_in: true
-    @auth = Authentication.new @params, request, session, @organization, false
 
     assert_invalid_authentication message: I18n.t('message.you_are_already_logged')
   end
 
   private
 
-    def assert_valid_authentication redirect_url: nil, message: nil
+    def assert_valid_authentication redirect_url: nil, message: nil, admin_mode: false
+      @auth = Authentication.new @params, request, session, @organization, admin_mode
+
       assert_difference 'LoginRecord.count' do
         assert @auth.authenticated?
         assert_equal redirect_url || Hash[controller: 'welcome', action: 'index'], @auth.redirect_url
@@ -116,7 +105,9 @@ class AuthenticationTest < ActionController::TestCase
       end
     end
 
-    def assert_invalid_authentication redirect_url: nil, message: nil
+    def assert_invalid_authentication redirect_url: nil, message: nil, admin_mode: false
+      @auth = Authentication.new @params, request, session, @organization, admin_mode
+
       assert_difference 'ErrorRecord.count' do
         assert !@auth.authenticated?
         assert_equal redirect_url || Hash[controller: 'sessions', action: 'new'], @auth.redirect_url
