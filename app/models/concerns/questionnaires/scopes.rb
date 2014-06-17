b9625c815eac378a7965c7b0a9e71ecd74ed4288
Ismael Isuani <ismaelisuani@gmail.com>
2014-06-17 11:16:39 -0300

    Improve questionnaires scopes

--- app/models/concerns/questionnaires/scopes.rb
+++ app/models/concerns/questionnaires/scopes.rb
@@ -4,9 +4,9 @@ module Questionnaires::Scopes
   included do
     scope :list, -> { where(organization_id: Organization.current_id) }
     scope :by_pollable_type, ->(type) { where(pollable_type: type) }
-    scope :pollable, -> { where('pollable_type IS NOT NULL') }
+    scope :pollable, -> { where.not(pollable_type: nil) }
     scope :by_organization, ->(org_id, id) {
-      unscoped.where('id = :id AND organization_id = :org_id', org_id: org_id, id: id)
+      unscoped.where(id: id, organization_id: org_id)
     }
   end
 end
