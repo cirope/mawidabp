6aadb6c1819c83c48956816fff944b7482b9340a
Ismael Isuani <ismaelisuani@gmail.com>
2014-07-03 09:35:21 -0300

    Improve poll answer options

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,4 +1,12 @@
 module PollsHelper
+  def answer_options_collection form
+    form.collection_radio_buttons(
+      :answer_option_id, answer_options(form.object.question), :first, :last,
+      item_wrapper_tag: :div,
+      item_wrapper_class: :radio
+    )
+  end
+
   def answer_options question
     question.answer_options.map do |o|
       [o.id, t("activerecord.attributes.answer_option.options.#{o.option}")]
