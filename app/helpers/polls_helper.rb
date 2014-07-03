5867c296671cdb3acb5b18f046e80875480ab6d8
Ismael Isuani <ismaelisuani@gmail.com>
2014-07-02 21:30:58 -0300

    Improve answer_options

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,6 +1,6 @@
 module PollsHelper
-  def answer_options form
-    form.object.question.answer_options.map do |o|
+  def answer_options question
+    question.answer_options.map do |o|
       [o.id, t("activerecord.attributes.answer_option.options.#{o.option}")]
     end
   end
