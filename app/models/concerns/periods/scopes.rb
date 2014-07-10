9940a21adf265a35c2bc4cdec3510f1e87e8a136
Ismael Isuani <ismaelisuani@gmail.com>
2014-07-10 12:15:35 -0300

    Period scopes refactor

--- app/models/concerns/periods/scopes.rb
+++ app/models/concerns/periods/scopes.rb
@@ -1,38 +1,42 @@
 module Periods::Scopes
   extend ActiveSupport::Concern
 
-  included do
-    scope :list, -> {
+  module ClassMethods
+    def list
       where(organization_id: Organization.current_id).order 'number DESC'
-    }
-    scope :list_by_date, ->(from_date, to_date) {
+    end
+
+    def list_by_date from_date, to_date
       list.where(
         [
           "#{table_name}.start BETWEEN :from_date AND :to_date",
           "#{table_name}.end BETWEEN :from_date AND :to_date"
         ].join(' OR '), { from_date: from_date, to_date: to_date }
-      ).reorder(["#{table_name}.start ASC", "#{table_name}.end ASC"])
-    }
-    scope :currents, -> {
+      ).reorder order_by_dates
+    end
+
+    def currents
       list.where(
         [
           "#{table_name}.start <= :today", "#{table_name}.end >= :today"
         ].join(' AND '), { today: Date.today }
-      ).reorder(["#{table_name}.start ASC", "#{table_name}.end ASC"])
-    }
-    scope :list_all_without_plans, -> {
-      list.includes(:plans).where(
-        "#{Plan.table_name}.period_id IS NULL"
-      ).reorder(["#{table_name}.start ASC", "#{table_name}.end ASC"]).references(
-        :plans
-      )
-    }
-    scope :list_all_without_procedure_controls, -> {
-      list.includes(:procedure_controls).where(
-        "#{ProcedureControl.table_name}.period_id IS NULL"
-      ).order(["#{table_name}.start ASC", "#{table_name}.end ASC"]).references(
-        :procedure_controls
-      )
-    }
+      ).reorder order_by_dates
+    end
+
+    def list_all_without_plans
+      list.includes(:plans).where(plans: { period_id: nil }).
+        reorder(order_by_dates).references(:plans)
+    end
+
+    def list_all_without_procedure_controls
+      list.includes(:procedure_controls).where(procedure_controls: { period_id: nil }).
+        order(order_by_dates).references(:procedure_controls)
+    end
+
+    private
+
+      def order_by_dates
+        ["#{table_name}.start ASC", "#{table_name}.end ASC"]
+      end
   end
 end
