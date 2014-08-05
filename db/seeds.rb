3fda717e309f2ac71af2bcd1c520df52f417a762
Franco Catena <francocatena@gmail.com>
2014-08-05 12:46:17 -0300

    Remove help content from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,6 @@ def self.check_errors(record)
 end
 
 User.paper_trail_off
-HelpContent.paper_trail_off
-HelpItem.paper_trail_off
 
 user = User.new(
   :name => 'Administrator',
@@ -31,21 +29,4 @@ user.save
 
 check_errors user
 
-help_content = HelpContent.new(:language => 'es')
-help_menus = ['Contenido', 'Acerca de Mawida']
-
-help_menus.each_with_index do |hi, i|
-  help_content.help_items << HelpItem.new(
-    :order_number => i.next,
-    :name => hi,
-    :description => '-'
-  )
-end
-
-help_content.save
-
-check_errors help_content
-
 User.paper_trail_on
-HelpContent.paper_trail_on
-HelpItem.paper_trail_on
