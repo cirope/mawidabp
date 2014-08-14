492ef6b3eefb97a28a0d0ff7ba1447591a8fd417
Franco Catena <francocatena@gmail.com>
2014-08-14 11:37:57 -0300

    Better db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,19 @@
-def self.check_errors(record)
-  if record.errors.size > 0
-    puts "Error in #{record.class}:"
-    record.errors.each { |error| p error }
-    return false
-  else
-    puts "#{record.class} ... [OK]"
-    return true
-  end
-end
-
-User.paper_trail_off
+User.paper_trail_off!
 
 user = User.new(
-  :name => 'Administrator',
-  :last_name => 'Administrator',
-  :language => 'es',
-  :email => SUPPORT_EMAIL,
-  :enable => true,
-  :password_changed => nil,
-  :user => 'admin',
-  :password => 'admin123'
+  name: 'Administrator',
+  last_name: 'Administrator',
+  language: 'es',
+  email: SUPPORT_EMAIL,
+  enable: true,
+  password_changed: nil,
+  user: 'admin',
+  password: 'admin123'
 )
 
 user.group_admin = true
 user.roles.each { |r| r.inject_auth_privileges Hash.new(Hash.new(true)) }
 user.encrypt_password
-user.save
+user.save!
 
-check_errors user
-
-User.paper_trail_on
+User.paper_trail_on!
