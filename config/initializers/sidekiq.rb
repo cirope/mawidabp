50e0516aaf07fa68b1549ae0964ee6c8e823a544
Franco Catena <francocatena@gmail.com>
2015-01-22 18:47:21 -0300

    Back to sidekiq original config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://localhost:6379/10', namespace: 'mawidabp' }
-# end
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://localhost:6379/10', namespace: 'mawidabp' }
+end
 
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: 'redis://localhost:6379/10', namespace: 'mawidabp' }
-# end
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost:6379/10', namespace: 'mawidabp' }
+end
