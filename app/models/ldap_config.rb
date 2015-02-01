70ddf157df8cc4a4ca3d6d9d6c531d9a8b9389da
Franco Catena <francocatena@gmail.com>
2015-01-31 21:16:13 -0300

    Extract ldap method to his own concern...

--- app/models/ldap_config.rb
+++ app/models/ldap_config.rb
@@ -1,17 +1,10 @@
 class LdapConfig < ActiveRecord::Base
   include Auditable
   include Trimmer
+  include LdapConfigs::LDAP
   include LdapConfigs::Validation
 
   trimmed_fields :hostname, :basedn, :username_ldap_attribute
 
   belongs_to :organization
-
-  def ldap username, password
-    Net::LDAP.new host: hostname, port: port, auth: {
-      method:   :simple,
-      username: "#{username_ldap_attribute}=#{username},#{basedn}",
-      password: password
-    }
-  end
 end
