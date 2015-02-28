dfb1ea8f88daddc998c70c623928dd58ecae9be6
Franco Catena <francocatena@gmail.com>
2015-02-28 01:59:15 -0300

    Use /run/unicorn to store the .sock file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,7 +15,7 @@ preload_app true
 # "current" directory that Capistrano sets up.
 working_directory app_path
 
-listen '/var/tmp/unicorn.sock', backlog: 1024
+listen '/run/unicorn/unicorn.sock', backlog: 1024
 
 # nuke workers after 180 seconds
 timeout 360
