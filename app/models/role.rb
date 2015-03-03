e4fddd7c403f7764265092174a9372ef10005740
Franco Catena <francocatena@gmail.com>
2015-03-03 10:59:31 -0300

    Remove unneeded format check in role's name

--- app/models/role.rb
+++ app/models/role.rb
@@ -35,8 +35,6 @@ class Role < ActiveRecord::Base
   before_save :check_change_in_privileges
 
   # Restricciones
-  validates :name, :format => {:with => /\A\w[\w\s-]*\z/},
-    :allow_nil => true, :allow_blank => true
   validates :name, :organization_id, :role_type, :presence => true
   validates :name, :length => {:maximum => 255}, :allow_nil => true,
     :allow_blank => true
