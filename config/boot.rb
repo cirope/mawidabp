ee3eb6d66d16d735812bed502a20ac7b16f00ba4
Franco Catena <francocatena@gmail.com>
2015-05-11 23:42:58 -0300

    Add default Oracle locale

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
+# Default Oracle locale
+ENV['NLS_LANG'] ||= 'LATIN AMERICAN SPANISH_ARGENTINA.AL32UTF8'
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
