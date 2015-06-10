2b41903ab2adf01fb0c1a12e258a8c7c18064510
Franco Catena <francocatena@gmail.com>
2015-06-10 09:18:07 -0300

    Remove unique constraint from user column...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427051208) do
+ActiveRecord::Schema.define(version: 20150610121431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -739,7 +739,7 @@ ActiveRecord::Schema.define(version: 20150427051208) do
   add_index "users", ["hidden"], name: "index_users_on_hidden", using: :btree
   add_index "users", ["manager_id"], name: "index_users_on_manager_id", using: :btree
   add_index "users", ["resource_id"], name: "index_users_on_resource_id", using: :btree
-  add_index "users", ["user"], name: "index_users_on_user", unique: true, using: :btree
+  add_index "users", ["user"], name: "index_users_on_user", using: :btree
 
   create_table "versions", force: true do |t|
     t.integer  "item_id"
