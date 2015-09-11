b29fcb65175758bcc47666d8ec7107f2a031c5bb
Franco Catena <francocatena@gmail.com>
2015-09-11 20:49:51 -0300

    Allow force approval parameter on drafts reviews

--- app/controllers/conclusion_draft_reviews_controller.rb
+++ app/controllers/conclusion_draft_reviews_controller.rb
@@ -282,7 +282,7 @@ class ConclusionDraftReviewsController < ApplicationController
     def conclusion_draft_review_params
       params.require(:conclusion_draft_review).permit(
         :review_id, :issue_date, :close_date, :applied_procedures, :conclusion,
-        :lock_version
+        :force_approval, :lock_version
       )
     end
 
