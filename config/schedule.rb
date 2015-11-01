2fec77dbc0dbdd947340c0e43a90049c9f79e7c7
Franco Catena <francocatena@gmail.com>
2015-11-01 11:58:55 -0300

    New schedule time, now at 8am

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,7 @@
 # Para eliminarla
 # whenever -c mawidabp
 
-every 1.day, at: '20:00' do
+every 1.day, at: '08:00' do
   runner 'ConclusionFinalReview.warning_auditors_about_close_date'
   runner 'Finding.notify_for_unconfirmed_for_notification_findings'
   runner 'User.notify_new_findings'
