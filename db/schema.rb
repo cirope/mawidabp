04be6692ccf2ffe5329e746206e1bde0308af0e3
Franco Catena <francocatena@gmail.com>
2016-09-17 23:06:50 -0300

    Remove old_object column from versions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915182823) do
+ActiveRecord::Schema.define(version: 20160918020315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -745,7 +745,6 @@ ActiveRecord::Schema.define(version: 20160915182823) do
     t.string   "item_type",       limit: 255
     t.string   "event",           limit: 255, null: false
     t.integer  "whodunnit"
-    t.text     "old_object"
     t.datetime "created_at"
     t.integer  "organization_id"
     t.boolean  "important"
