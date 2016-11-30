cc1877251596533c4fc228747cfdfb5da4c39dd8
Franco Catena <francocatena@gmail.com>
2016-11-30 16:28:54 -0300

    Wrap tags JS code on jQuery _ready_ function

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,10 +1,12 @@
-$(document).on('click', '[data-icon]', function (event) {
-  event.preventDefault()
+jQuery(function ($) {
+  $(document).on('click', '[data-icon]', function (event) {
+    event.preventDefault()
 
-  var icon = $(this).data('icon')
+    var icon = $(this).data('icon')
 
-  $('[data-icon]').closest('li').removeClass('active')
-  $('[data-icon="' + icon + '"]').closest('li').addClass('active')
+    $('[data-icon]').closest('li').removeClass('active')
+    $('[data-icon="' + icon + '"]').closest('li').addClass('active')
 
-  $('#tag_icon').val(icon)
+    $('#tag_icon').val(icon)
+  })
 })
