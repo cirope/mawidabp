842734e67c75d09e37b214a9c208cd942b2c7612
Franco Catena <francocatena@gmail.com>
2016-12-08 17:55:03 -0300

    Move EffectHelper to dynamic_form.js

--- app/assets/javascripts/dynamic_form.js
+++ app/assets/javascripts/dynamic_form.js
@@ -1,91 +1,104 @@
-var DynamicFormEvent = {
-  addNestedItem: function ($e) {
-    console.log('addNestedItem')
-    var template    = $e.data('dynamicTemplate')
-    var regexp      = new RegExp($e.data('id'), 'g')
-    var partial     = DynamicFormHelper.replaceIds(template, regexp)
-    var $insertInto = $($e.data('insertInto'))
++function () {
+  var EffectHelper = {
+    hide: function (element, callback) {
+      $(element).stop().fadeOut(200, callback)
+    },
 
-    if ($insertInto.length)
-      $insertInto.append(partial)
-    else
-      $e.before(partial)
+    remove: function (element, callback) {
+      $(element).stop().fadeOut(200, function () {
+        setTimeout(function () {
+          $(element).remove()
+        }, 200)
 
-    $e.trigger('dynamic-item.added', $e)
-  },
+        if (typeof callback === 'function') callback()
+      })
+    }
+  }
 
-  insertNestedItem: function ($e) {
-    console.log('insertNestedItem')
-    var source          = $e.data('dynamicSource')
-    var $templateHolder = DynamicFormHelper.findInNearestFieldset($e, source)
-    var template        = $templateHolder.data('dynamicTemplate')
-    var regexp          = new RegExp($templateHolder.data('id'), 'g')
+  var DynamicFormEvent = {
+    addNestedItem: function ($e) {
+      var template    = $e.data('dynamicTemplate')
+      var regexp      = new RegExp($e.data('id'), 'g')
+      var partial     = DynamicFormHelper.replaceIds(template, regexp)
+      var $insertInto = $($e.data('insertInto'))
 
-    $e.closest('fieldset').before(DynamicFormHelper.replaceIds(template, regexp))
+      if ($insertInto.length)
+        $insertInto.append(partial)
+      else
+        $e.before(partial)
 
-    $e.trigger('dynamic-item.added', $e)
-  },
+      $e.trigger('dynamic-item.added', $e)
+    },
 
-  hideItem: function ($e) {
-    console.log('hideItem')
-    EffectHelper.hide($e.closest('fieldset'))
+    insertNestedItem: function ($e) {
+      var source          = $e.data('dynamicSource')
+      var $templateHolder = DynamicFormHelper.findInNearestFieldset($e, source)
+      var template        = $templateHolder.data('dynamicTemplate')
+      var regexp          = new RegExp($templateHolder.data('id'), 'g')
 
-    $e.prev('input[type=hidden].destroy').val('1').trigger(
-      'dynamic-item.hidden', $e
-    )
-  },
+      $e.closest('fieldset').before(DynamicFormHelper.replaceIds(template, regexp))
 
-  removeItem: function ($e) {
-    console.log('removeItem')
-    EffectHelper.remove($e.closest('fieldset'), function () {
-      $e.trigger('dynamic-item.removed', $e)
+      $e.trigger('dynamic-item.added', $e)
+    },
+
+    hideItem: function ($e) {
+      EffectHelper.hide($e.closest('fieldset'))
+
+      $e.prev('input[type=hidden].destroy').val('1').trigger(
+        'dynamic-item.hidden', $e
+      )
+    },
+
+    removeItem: function ($e) {
+      EffectHelper.remove($e.closest('fieldset'), function () {
+        $e.trigger('dynamic-item.removed', $e)
+      })
+    }
+  }
+
+  var DynamicFormHelper = {
+    newIdCounter: 0,
+
+    replaceIds: function (s, regex) {
+      return s.replace(regex, new Date().getTime() + DynamicFormHelper.newIdCounter++)
+    },
+
+    findInNearestFieldset: function (element, selector) {
+      var $fieldset = $(element).parents('fieldset')
+      var result    = $fieldset.find(selector)
+
+      while ($fieldset.length && ! result.length) {
+        $fieldset = $fieldset.parents('fieldset')
+        result    = $fieldset.find(selector)
+      }
+
+      return result.length && result || $(selector)
+    }
+  }
+
+  jQuery(function ($) {
+    var linkSelector = 'a[data-dynamic-form-event]'
+    var eventList    = $.map(DynamicFormEvent, function (v, k) { return k })
+
+    $(document).on('click', linkSelector, function (event) {
+      if (event.stopped) return
+
+      var eventName = $(this).data('dynamicFormEvent')
+
+      if ($.inArray(eventName, eventList) !== -1) {
+        DynamicFormEvent[eventName]($(this))
+
+        event.preventDefault()
+        event.stopPropagation()
+      }
     })
-  }
-}
 
-var DynamicFormHelper = {
-  newIdCounter: 0,
+    $(document).on('dynamic-item.added', linkSelector, function (event, element) {
+      $(element).prev('fieldset').find(
+        '[autofocus]:not([readonly]):enabled:visible:first'
+      ).focus()
+    })
 
-  replaceIds: function (s, regex) {
-    console.log('replaceIds')
-    return s.replace(regex, new Date().getTime() + DynamicFormHelper.newIdCounter++)
-  },
-
-  findInNearestFieldset: function (element, selector) {
-    var $fieldset = $(element).parents('fieldset')
-    var result    = $fieldset.find(selector)
-
-    while ($fieldset.length && ! result.length) {
-      $fieldset = $fieldset.parents('fieldset')
-      result    = $fieldset.find(selector)
-    }
-
-    return result.length && result || $(selector)
-  }
-}
-
-jQuery(function ($) {
-  var linkSelector = 'a[data-dynamic-form-event]'
-  var eventList    = $.map(DynamicFormEvent, function (v, k) { return k })
-
-  $(document).on('click', linkSelector, function (event) {
-    if (event.stopped) return
-
-    var eventName = $(this).data('dynamicFormEvent')
-
-    if ($.inArray(eventName, eventList) !== -1) {
-      DynamicFormEvent[eventName]($(this))
-
-      event.preventDefault()
-      event.stopPropagation()
-    }
+    $('[name$="[_destroy]"][value=1]').closest('fieldset').hide()
   })
-
-  $(document).on('dynamic-item.added', linkSelector, function (event, element) {
-    $(element).prev('fieldset').find(
-      '[autofocus]:not([readonly]):enabled:visible:first'
-    ).focus()
-  })
-
-  $('[name$="[_destroy]"][value=1]').closest('fieldset').hide()
-})
+}()
