988523085391a04fd2e175360f7c307e3789c3f4
Franco Catena <francocatena@gmail.com>
2017-01-03 13:50:12 -0300

    Remove file model presence validation on file attr

--- app/models/concerns/file_models/validation.rb
+++ app/models/concerns/file_models/validation.rb
@@ -2,8 +2,7 @@ module FileModels::Validation
   extend ActiveSupport::Concern
 
   included do
-    validates :file_file_name, :file_content_type, presence: true,
-      length: { maximum: 255 }
+    validates :file_file_name, :file_content_type, length: { maximum: 255 }
     validates_each :file do |record, attr, value|
       if record.identifier && File.extname(record.identifier).blank?
         record.errors.add attr, :without_extension
