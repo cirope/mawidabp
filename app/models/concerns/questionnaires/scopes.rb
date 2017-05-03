c9203a1b3bbf9f647a34de962536250e6557f591
Franco Catena <francocatena@gmail.com>
2017-05-03 14:50:32 -0300

    Better questionnaires scopes _style_

--- app/models/concerns/questionnaires/scopes.rb
+++ app/models/concerns/questionnaires/scopes.rb
@@ -2,12 +2,22 @@ module Questionnaires::Scopes
   extend ActiveSupport::Concern
 
   included do
-    scope :list, -> { where(organization_id: Organization.current_id) }
-    scope :by_pollable_type, ->(type) { where(pollable_type: type) }
-    scope :pollable, -> { where.not(pollable_type: nil).where.not(pollable_type: '') }
-    scope :not_pollable, -> { where(pollable_type: nil).or(where(pollable_type: '')) }
-    scope :by_organization, ->(org_id, id) {
-      unscoped.where(id: id, organization_id: org_id)
+    scope :list, -> { where organization_id: Organization.current_id }
+    scope :pollable, -> {
+      where.not(pollable_type: nil).where.not(pollable_type: '')
     }
+    scope :not_pollable, -> {
+      where(pollable_type: nil).or where(pollable_type: '')
+    }
+  end
+
+  module ClassMethods
+    def by_pollable_type type
+      where pollable_type: type
+    end
+
+    def by_organization organization_id, id
+      unscoped.where id: id, organization_id: organization_id
+    end
   end
 end
