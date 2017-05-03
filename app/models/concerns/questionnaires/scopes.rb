debe7ec2f3a2fdca6dd81e63b72edd299204b994
Franco Catena <francocatena@gmail.com>
2017-05-03 14:45:18 -0300

    Fix pollable_type related bugs on Polls

--- app/models/concerns/questionnaires/scopes.rb
+++ app/models/concerns/questionnaires/scopes.rb
@@ -4,7 +4,8 @@ module Questionnaires::Scopes
   included do
     scope :list, -> { where(organization_id: Organization.current_id) }
     scope :by_pollable_type, ->(type) { where(pollable_type: type) }
-    scope :pollable, -> { where.not(pollable_type: nil) }
+    scope :pollable, -> { where.not(pollable_type: nil).where.not(pollable_type: '') }
+    scope :not_pollable, -> { where(pollable_type: nil).or(where(pollable_type: '')) }
     scope :by_organization, ->(org_id, id) {
       unscoped.where(id: id, organization_id: org_id)
     }
