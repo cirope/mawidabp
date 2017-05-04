9e68ed32a3e981d86b2e1a1e3026a817840a933d
Franco Catena <francocatena@gmail.com>
2017-05-03 21:43:34 -0300

    Assert findings PDF redirect with more bits

--- test/controllers/findings_controller_test.rb
+++ test/controllers/findings_controller_test.rb
@@ -161,7 +161,7 @@ class FindingsControllerTest < ActionController::TestCase
       :completed => 'incomplete',
       :format => :pdf
     }
-    assert_redirected_to /\/private\/.*\.pdf$/
+    assert_redirected_to /\/private\/.*\/findings\/.*\.pdf$/
     assert_equal "#{Mime[:pdf]}", @response.content_type
   end
 
