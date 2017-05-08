028f44759d91b696717728becfa24c16ec120d7f
Franco Catena <francocatena@gmail.com>
2017-05-08 14:35:09 -0300

    Plan form aesthetical improvements

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -1,9 +1,14 @@
 module PlansHelper
-  def plan_resource_field form, id = '', inline = true
-    form.input :resource_id, collection: ResourceClass.list, as: :grouped_select,
-      group_method: :resources, label: false, prompt: true, input_html: {
-      id: "#{id}_resource_id", autofocus: true
-    }
+  def plan_resource_field form, id = nil
+    form.input :resource_id,
+      collection: ResourceClass.list,
+      as: :grouped_select,
+      group_method: :resources,
+      label: false,
+      prompt: true,
+      input_html: {
+        id: "#{id}_resource_id", autofocus: true
+      }
   end
 
   def show_plan_item_info plan_item
@@ -34,28 +39,12 @@ module PlansHelper
     (BusinessUnitType.list + [nil]).map { |but| [but, grouped_plan_items[but]] }
   end
 
-  def show_plan_business_unit_type_info
-    label = @business_unit_type&.name || t('plans.without_business_unit_type')
-    link  = @plan.new_record? ? new_plan_path : edit_plan_path(@plan)
-
-    content_tag :h4, raw("#{label} - #{link_to t('.show_all'), link}")
-  end
-
-  def link_to_plan_business_unit_type but, plan_items
-    label   = but&.name || t('plans.without_business_unit_type')
-    classes = ['list-group-item']
-    badge   = content_tag :span, plan_items&.size || 0, class: 'badge'
-
-    if @plan.new_record?
-      url = '#'
-
-      classes << 'disabled'
+  def link_to_plan_business_unit_type but, &block
+    if @plan.persisted?
+      link_to edit_plan_path(@plan, business_unit_type: but || 'nil'), &block
     else
-      url = edit_plan_path(@plan, business_unit_type: but || 'nil')
+      link_to '#', &block
     end
 
-    link_to url, class: classes.join(' ') do
-      raw "#{badge} #{label}"
-    end
   end
 end
