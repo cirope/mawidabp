f67c8e2dc436974d81d84076a357e31611c9fc9a
Franco Catena <francocatena@gmail.com>
2017-05-23 01:52:57 -0300

    Upper limit validation on decimal attributes

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -9,8 +9,10 @@ class Cost < ApplicationRecord
   validates :cost, :cost_type, :user_id, :item_id, :item_type, :presence => true
   validates :user_id, :item_id, :numericality => {:only_integer => true},
     :allow_nil => true, :allow_blank => true
-  validates :cost, :numericality => {:greater_than_or_equal_to => 0},
-    :allow_nil => true, :allow_blank => true
+  validates :cost, :numericality => {
+    :greater_than_or_equal_to => 0,
+    :less_than_or_equal_to => 9_999_999_999_999.99
+  }, :allow_nil => true, :allow_blank => true
   validates :description, :pdf_encoding => true
 
   # Relaciones
