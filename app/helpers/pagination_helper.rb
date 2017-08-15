67df473f2cf9331e07c5bdce5fadd59b83d664cf
Franco Catena <francocatena@gmail.com>
2017-08-15 15:48:59 -0300

    Change deprecated pagination helper gem
    will_paginate-bootstrap for bootstrap-will_paginate

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -1,6 +1,5 @@
 module PaginationHelper
   def paginate collection
-    will_paginate collection, class: 'pull-right small pagination-sm',
-      renderer: BootstrapPagination::Rails
+    will_paginate collection, class: 'pull-right small pagination-sm'
   end
 end
