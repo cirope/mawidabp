974fcb70da74f6209caaf074ea75461c6a15c5ae
Franco Catena <francocatena@gmail.com>
2017-09-18 12:12:31 -0300

    New Galicia customer testing branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,16 @@
-set :branch,    'master'
+set :branch,    'c-galicia-test'
 set :stage,     :production
 set :rails_env, 'production'
 
 set :default_env, {
+  'PATH':     '$PATH:/usr/local/bin/',
   'LANG':     'en_US.UTF-8',
   'LANGUAGE': 'en_US.UTF-8',
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@app.mawidabp.com}
-role :app, %w{deployer@app.mawidabp.com}
-role :db,  %w{deployer@app.mawidabp.com}
+role :web, %w{deployer@galicia.mawidabp.com}
+role :app, %w{deployer@galicia.mawidabp.com}
+role :db,  %w{deployer@galicia.mawidabp.com}
 
-server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'galicia.mawidabp.com', user: 'deployer', roles: %w{web app db}
