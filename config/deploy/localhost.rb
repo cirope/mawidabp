665fc78386baa4de62f3b6137dc363ada6185a6f
Franco Catena <francocatena@gmail.com>
2017-10-02 10:48:22 -0300

    _Blend_ localhost deploy config ENV variables

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -4,13 +4,10 @@ set :rails_env, 'production'
 
 set :default_env, {
   'http_proxy':  'http://192.168.101.101:8080',
-  'https_proxy': 'http://192.168.101.101:8080'
-}
-
-set :default_env, {
-  'LANG':     'en_US.UTF-8',
-  'LANGUAGE': 'en_US.UTF-8',
-  'LC_ALL':   'en_US.UTF-8'
+  'https_proxy': 'http://192.168.101.101:8080',
+  'LANG':        'en_US.UTF-8',
+  'LANGUAGE':    'en_US.UTF-8',
+  'LC_ALL':      'en_US.UTF-8'
 }
 
 role :web, %w{deployer@127.0.0.1}
