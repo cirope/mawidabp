f9e1d4488cff5da7109cbe709bf556589febcb06
Franco Catena <francocatena@gmail.com>
2017-10-10 19:03:03 -0300

    Validate review code prefix on a separated method

--- app/models/concerns/weaknesses/validations.rb
+++ app/models/concerns/weaknesses/validations.rb
@@ -4,10 +4,14 @@ module Weaknesses::Validations
   included do
     validates :risk, :priority, presence: true
     validates :audit_recommendations, presence: true, if: :notify?
-    validates_each :review_code do |record, attr, value|
-      regex = /\A#{record.prefix}\d+\Z/
-
-      record.errors.add attr, :invalid unless value =~ regex
-    end
+    validate :review_code_has_valid_prefix
   end
+
+  private
+
+    def review_code_has_valid_prefix
+      regex = /\A#{prefix}\d+\Z/
+
+      errors.add :review_code, :invalid unless review_code =~ regex
+    end
 end
