5610529d29d9d0b64f95a9dbf1f240403843f8d5
Franco Catena <francocatena@gmail.com>
2017-10-23 15:10:10 -0300

    Add same ENV variables from production to testing

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -3,6 +3,12 @@ set :rails_env, 'production'
 set :branch, 'c-petersen-test'
 set :pty, true
 
+set :default_env, {
+  'LANG':     'en_US.UTF-8',
+  'LANGUAGE': 'en_US.UTF-8',
+  'LC_ALL':   'en_US.UTF-8'
+}
+
 role :web, %w{deployer@application.tmawidabp.petersen.corp}
 role :app, %w{deployer@application.tmawidabp.petersen.corp}
 role :db,  %w{deployer@application.tmawidabp.petersen.corp}
