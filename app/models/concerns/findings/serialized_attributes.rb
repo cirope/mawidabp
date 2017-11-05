103a3bfd414f8c5498e7490734e960dcebaf4f89
Franco Catena <francocatena@gmail.com>
2017-11-05 20:30:52 -0300

    Remove some Oracle + serialize madness

--- app/models/concerns/findings/serialized_attributes.rb
+++ app/models/concerns/findings/serialized_attributes.rb
@@ -2,7 +2,7 @@ module Findings::SerializedAttributes
   extend ActiveSupport::Concern
 
   included do
-    if SHOW_WEAKNESS_EXTRA_ATTRIBUTES && connection.adapter_name != 'PostgreSQL'
+    unless connection.adapter_name == 'PostgreSQL'
       serialize :impact, JSON
       serialize :internal_control_components, JSON
     end
