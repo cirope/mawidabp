3014146f2b026d6c8880a7c6397a674316c5c737
Franco Catena <francocatena@gmail.com>
2017-12-28 17:19:33 -0300

    "Backport" operational risk migration for Oracle

--- db/migrate/20171121164102_change_operational_risk_to_array_on_findings.rb
+++ db/migrate/20171121164102_change_operational_risk_to_array_on_findings.rb
@@ -3,6 +3,10 @@ class ChangeOperationalRiskToArrayOnFindings < ActiveRecord::Migration[5.1]
     if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
       change_column :findings, :operational_risk, :text, array: true, default: [],
         using: "(string_to_array(operational_risk, ','))"
+    elsif ActiveRecord::Base.connection.adapter_name == 'OracleEnhanced'
+      # Don't care about data, Oracle users don't have this feature enabled
+      remove_column :findings, :operational_risk
+      add_column :findings, :operational_risk, :text, array: true, default: [].to_json
     else
       change_column :findings, :operational_risk, :text, array: true, default: [].to_json
     end
