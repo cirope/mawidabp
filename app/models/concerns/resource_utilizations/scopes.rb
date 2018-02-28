402f67187f47171caa2ff1fe0d3a7ef82227d7e8
Franco Catena <francocatena@gmail.com>
2018-02-28 15:44:47 -0300

    New planned cost summary on execution reports

--- app/models/concerns/resource_utilizations/scopes.rb
+++ app/models/concerns/resource_utilizations/scopes.rb
@@ -7,12 +7,20 @@ module ResourceUtilizations::Scopes
   end
 
   module ClassMethods
-    def planed_on reviews
+    def planned_on reviews
       joins(:planed_review).where reviews: { id: reviews.map(&:id) }
     end
 
     def executed_on reviews
       joins(:review).where reviews: { id: reviews.map(&:id) }
+    end
+
+    def items_planned_on plan_items
+      joins(:plan_item).where plan_items: { id: plan_items.map(&:id) }
+    end
+
+    def items_executed_on plan_items
+      joins(:review).where reviews: { plan_item_id: plan_items.map(&:id) }
     end
   end
 end
