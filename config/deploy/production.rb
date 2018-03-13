f746ad8524dad69921e197b8639f18fb67e8208f
Franco Catena <francocatena@gmail.com>
2018-03-12 21:32:21 -0300

    New customer

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,    'master'
+set :branch,    'c-undec'
 set :stage,     :production
 set :rails_env, 'production'
 
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@app.mawidabp.com}
-role :app, %w{deployer@app.mawidabp.com}
-role :db,  %w{deployer@app.mawidabp.com}
+role :web, %w{deployer@undec.mawidabp.com}
+role :app, %w{deployer@undec.mawidabp.com}
+role :db,  %w{deployer@undec.mawidabp.com}
 
-server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'undec.mawidabp.com', user: 'deployer', roles: %w{web app db}
