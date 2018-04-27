2535d9204a7b80a0f4b2aa227535ef4b4dc06469
Franco Catena <francocatena@gmail.com>
2018-04-27 10:20:03 -0300

    Add new filters to weaknesses by risk test

--- test/controllers/follow_up_audit_controller_test.rb
+++ test/controllers/follow_up_audit_controller_test.rb
@@ -244,7 +244,9 @@ class FollowUpAuditControllerTest < ActionController::TestCase
       get :weaknesses_by_risk, :params => {
         :weaknesses_by_risk => {
           :from_date => 10.years.ago.to_date,
-          :to_date => 10.years.from_now.to_date
+          :to_date => 10.years.from_now.to_date,
+          :compliance => 'yes',
+          :repeated => 'false'
         },
         :controller_name => 'follow_up',
         :final => false
