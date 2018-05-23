f63140d90805b6b4ceefc8f20198cd83d8050013
Néstor Coppi <nestorcoppi@gmail.com>
2018-05-22 21:38:39 -0300

    Chau comentario

--- app/models/concerns/ldap_configs/ldap_service.rb
+++ app/models/concerns/ldap_configs/ldap_service.rb
@@ -2,7 +2,6 @@ module LdapConfigs::LDAPService
   extend ActiveSupport::Concern
 
   included do
-    # attr_accessor :password
     before_save :encrypt_password, if: ->(ldap) { ldap.password.present? }
 
     scope :with_user, -> { where.not(encrypted_password: nil) }
