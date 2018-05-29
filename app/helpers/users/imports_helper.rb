37d85a486320de0d8a0692cd3c0ae398cd82abc8
Néstor Coppi <nestorcoppi@gmail.com>
2018-05-29 20:31:37 -0300

    Label for state

--- app/helpers/users/imports_helper.rb
+++ app/helpers/users/imports_helper.rb
@@ -1,2 +1,10 @@
 module Users::ImportsHelper
+  def users_import_state_label_class(state)
+    case state
+      when :created   then 'label-success'
+      when :updated   then 'label-info'
+      when :deleted   then 'label-warning'
+      when :unchanged then 'label-default'
+    end
+  end
 end
