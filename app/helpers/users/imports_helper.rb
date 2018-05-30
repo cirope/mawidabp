17c21da2c22368cb2ce35ed2c2d2ddd73891e725
Néstor Coppi <nestorcoppi@gmail.com>
2018-05-29 23:25:39 -0300

    Sale con hash

--- app/helpers/users/imports_helper.rb
+++ app/helpers/users/imports_helper.rb
@@ -1,10 +1,10 @@
 module Users::ImportsHelper
   def users_import_state_label_class(state)
-    case state
-      when :created   then 'label-success'
-      when :updated   then 'label-info'
-      when :deleted   then 'label-warning'
-      when :unchanged then 'label-default'
-    end
+    {
+      created:   'label-success',
+      updated:   'label-info',
+      deleted:   'label-warning',
+      unchanged: 'label-default'
+    }[state]
   end
 end
