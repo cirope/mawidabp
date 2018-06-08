891c8087ed4bb38ffd5639244b0e2d145345f799
Néstor Coppi <nestorcoppi@gmail.com>
2018-06-07 22:39:55 -0300

    Ldap user sync class method (#73)

--- app/mailers/ldap_mailer.rb
+++ app/mailers/ldap_mailer.rb
@@ -1,18 +1,25 @@
 class LdapMailer < ActionMailer::Base
   include ActionView::Helpers::TextHelper
 
+  helper :application
+
   default from: "#{ENV['EMAIL_NAME'] || I18n.t('app_name')} <#{ENV['EMAIL_ADDRESS']}>"
 
-  def import_notifier(imported_users, organization)
+  def import_notifier(imported_users_json, organization_id)
     @users = {
-      created:   [],
-      updated:   [],
-      deleted:   []
+      created: [],
+      deleted: [],
+      errored: [],
+      updated: []
     }
 
+    organization = Organization.find(organization_id)
+    imported_users = JSON.parse(imported_users_json)
+
     imported_users.each do |d|
-      @users[d[:state]] << d[:user] unless d[:state] == :unchanged # unused
+      @users[d['state'].to_sym] << d['user']
     end
+    @users.reject! { |_k, v| v.empty? }
 
     emails = organization.users_with_roles(:supervisor, :manager).pluck(:email)
     subject = I18n.t(
--- app/models/concerns/ldap_configs/ldap_service.rb
+++ app/models/concerns/ldap_configs/ldap_service.rb
@@ -2,6 +2,8 @@ module LdapConfigs::LDAPService
   extend ActiveSupport::Concern
 
   included do
+    scope :with_user, -> { where.not(user: nil, encrypted_password: nil) }
+
     before_save :encrypt_password, if: :password?
   end
 
@@ -11,6 +13,37 @@ module LdapConfigs::LDAPService
 
   def decrypted_password
     Security.decrypt(encrypted_password) if encrypted_password.present?
+  end
+
+  def sync
+    import(user, decrypted_password)
+  end
+
+  module ClassMethods
+    def sync_users
+      with_user.preload(:organization).each do |ldap|
+        organization = ldap.organization
+
+        Organization.current_id = organization.id # Roles scope
+
+        ::Rails.logger.info(
+          "[#{organization.prefix.upcase}] Importing users for #{ldap.basedn}"
+        )
+
+        imports = ldap.sync
+        filtered_imports = imports.map do |i|
+          unless i[:state] == :unchanged
+            { user: { name: i[:user].to_s, errors: i[:errors] }, state: i[:state] }
+          end
+        end.compact
+
+        if filtered_imports.any?
+          LdapMailer.import_notifier(filtered_imports.to_json, organization.id).deliver_later
+        end
+      end
+
+      Organization.current_id = nil
+    end
   end
 
   private
