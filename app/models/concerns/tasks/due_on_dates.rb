2b900d11c9389fbbc8a599cf2282b6e567447958
Franco Catena <francocatena@gmail.com>
2018-07-03 18:16:21 -0300

    Let's use reify(dup: true) on task due on dates

--- app/models/concerns/tasks/due_on_dates.rb
+++ app/models/concerns/tasks/due_on_dates.rb
@@ -4,7 +4,7 @@ module Tasks::DueOnDates
   def all_due_on_dates
     all_due_on_dates = []
     last_date        = due_on
-    dates            = versions.map { |v| v.reify&.due_on }
+    dates            = versions.map { |v| v.reify(dup: true)&.due_on }
 
     dates.each do |d|
       all_due_on_dates << last_date = d if d.present? && d != last_date
