26f0776bf7f38ab8e9175577c308fad1e773091b
Néstor Coppi <nestorcoppi@gmail.com>
2018-08-24 00:32:37 -0300

    Questionnaires un-destroyable with answered polls

--- app/models/concerns/questionnaires/validations.rb
+++ app/models/concerns/questionnaires/validations.rb
@@ -9,5 +9,16 @@ module Questionnaires::Validations
       :email_clarification, pdf_encoding: true
     validates :name, :email_subject, :email_link, length: { maximum: 255 },
       allow_nil: true, allow_blank: true
+
+    before_destroy :check_for_answered_polls, prepend: true
   end
+
+  private
+
+    def check_for_answered_polls
+      unless polls.answered(true).count.zero?
+        errors.add(:base, :cannot_destroy_with_answered_poll)
+        throw :abort
+      end
+    end
 end
