db4c138b7350ac5032fab0d585d0f678a912f75c
Franco Catena <francocatena@gmail.com>
2018-12-03 11:40:42 -0300

    Rename mark as reschedule if applies to singular

--- app/models/concerns/findings/reschedule.rb
+++ app/models/concerns/findings/reschedule.rb
@@ -2,10 +2,10 @@ module Findings::Reschedule
   extend ActiveSupport::Concern
 
   included do
-    before_save :mark_as_rescheduled_if_applies
+    before_save :mark_as_rescheduled_if_apply
   end
 
-  def mark_as_rescheduled_if_applies
+  def mark_as_rescheduled_if_apply
     self.rescheduled ||= follow_up_date_changed? &&
       follow_up_date_was.present? &&
       final_review_created_at.present?
