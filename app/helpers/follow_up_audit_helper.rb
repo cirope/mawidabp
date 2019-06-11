b076fe6d3290eef1241007d9d8a70e8cfdefa34e
Franco Catena <francocatena@gmail.com>
2019-06-11 18:34:10 -0300

    Show only owners on weaknesses brief report

--- app/helpers/follow_up_audit_helper.rb
+++ app/helpers/follow_up_audit_helper.rb
@@ -2,7 +2,7 @@ module FollowUpAuditHelper
   def weaknesses_brief_audit_users weakness
     weakness.
       finding_user_assignments.
-      select { |fua| fua.user.can_act_as_audited? }.
+      select { |fua| fua.process_owner && fua.user.can_act_as_audited? }.
       map(&:user).
       map(&:full_name_with_function)
   end
