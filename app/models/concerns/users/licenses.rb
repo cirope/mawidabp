15c471e74c4482a9f5dfc28143f8c48fa2fb4625
Néstor Coppi <nestorcoppi@gmail.com>
2019-10-06 20:21:01 -0300

    With new_auditor? method

--- app/models/concerns/users/licenses.rb
+++ app/models/concerns/users/licenses.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Users::Licenses
   extend ActiveSupport::Concern
 
@@ -12,9 +13,18 @@ module Users::Licenses
     end
 
     def check_auditors_limit_on_role_change
+      if adding_new_auditor? && !Current.group.can_create_auditor?
+        Rails.logger.info "Group auditors limit reached (#{Current.group.auditors_limit})"
+        errors.add :base, :auditors_limit_reached
+
+        throw :abort
+      end
+    end
+
+    def adding_new_auditor?
       old_user = nil
 
-      new_auditor = organization_roles.reject(&:marked_for_destruction?).any? do |organization_role|
+      organization_roles.reject(&:marked_for_destruction?).any? do |organization_role|
         audited = if new_record?
                     true
                   else
@@ -24,13 +34,6 @@ module Users::Licenses
                   end
 
         audited && Role::ACT_AS[:auditor].include?(organization_role.role.role_type)
-      end
-
-      if new_auditor && !Current.group.can_create_auditor?
-        Rails.logger.info "Group auditors limit reached (#{Current.group.auditors_limit})"
-        errors.add :base, :auditors_limit_reached
-
-        throw :abort
       end
     end
 end
