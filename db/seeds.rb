d7e37d2f87a0e3cde05cb3e060b006ac3e3cc99a
Franco Catena <francocatena@gmail.com>
2019-11-01 09:48:15 -0300

    Only run seed when no admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,19 @@
-PaperTrail.request enabled: false do
-  user = User.new(
-    name:             'Administrator',
-    last_name:        'Administrator',
-    language:         'es',
-    email:            SUPPORT_EMAIL,
-    enable:           true,
-    password_changed: nil,
-    user:             'admin',
-    password:         'admin123'
-  )
+if User.where(user: 'admin').empty?
+  PaperTrail.request enabled: false do
+    user = User.new(
+      name:             'Administrator',
+      last_name:        'Administrator',
+      language:         'es',
+      email:            SUPPORT_EMAIL,
+      enable:           true,
+      password_changed: nil,
+      user:             'admin',
+      password:         'admin123'
+    )
 
-  user.group_admin = true
-  user.roles.each { |r| r.inject_auth_privileges Hash.new(Hash.new(true)) }
-  user.encrypt_password
-  user.save!
+    user.group_admin = true
+    user.roles.each { |r| r.inject_auth_privileges Hash.new(Hash.new(true)) }
+    user.encrypt_password
+    user.save!
+  end
 end
