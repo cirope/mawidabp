389307a3631f2f355500651300fceb658c1f5ef1
Franco Catena <francocatena@gmail.com>
2020-01-22 21:53:22 -0300

    Aesthetic tweaks on weaknesses report pagination

--- app/assets/javascripts/endless_pagination.js
+++ app/assets/javascripts/endless_pagination.js
@@ -1,12 +1,13 @@
 jQuery(function ($) {
   if ($('.endless-pagination').length) {
     $(document).on('scroll touchmove', function () {
-      var pagination = $('.pagination')
-      var url        = pagination.find('.next_page a').attr('href')
-      var atBottom   = $(window).scrollTop() > $(document).height() - $(window).height() - 50
+      var $pagination = $('.pagination')
+      var url         = $pagination.find('.next_page a').attr('href')
+      var atBottom    = $(window).scrollTop() > $(document).height() - $(window).height() - 50
 
       if (url && atBottom && !$.active) { // skip double call
-        pagination.text($('.loading-caption').text().trim())
+        $pagination.text('')
+        $('[data-pagination-loading]').removeAttr('hidden')
         $.getScript(url, function () { $(document).scroll() })
       }
     })
