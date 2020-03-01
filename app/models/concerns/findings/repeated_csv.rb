1715465637fcb3aa0e367b36bcb8ff43f1506dcc
Franco Catena <francocatena@gmail.com>
2020-03-01 13:57:14 -0300

    Suggested tweaks to weaknesses repeated report

--- app/models/concerns/findings/repeated_csv.rb
+++ app/models/concerns/findings/repeated_csv.rb
@@ -26,12 +26,13 @@ module Findings::RepeatedCsv
           PlanItem.human_attribute_name('project'),
           I18n.t('follow_up_committee_report.weaknesses_repeated.origination_year'),
           "#{repeated_text} - #{Weakness.human_attribute_name 'risk'}",
-          Weakness.human_attribute_name('title'),
+          "#{repeated_text} - #{Weakness.human_attribute_name('title')}",
           Weakness.human_attribute_name('description'),
           "#{repeated_text} - #{Weakness.human_attribute_name 'answer'}",
           Review.model_name.human,
           Weakness.human_attribute_name('risk'),
           Weakness.human_attribute_name('state'),
+          Weakness.human_attribute_name('title'),
           Weakness.human_attribute_name('answer'),
           Weakness.human_attribute_name('follow_up_date')
         ]
@@ -52,6 +53,7 @@ module Findings::RepeatedCsv
             current_weakness.review.identification,
             current_weakness.risk_text,
             current_weakness.state_text,
+            current_weakness.title,
             current_weakness.answer,
             (I18n.l current_weakness.follow_up_date if weakness.follow_up_date)
           ]
