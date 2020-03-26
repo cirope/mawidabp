f0f775477b6810594a7026ccf8b38ea0df7c49bd
Néstor Coppi <nestorcoppi@gmail.com>
2020-03-25 22:58:18 -0300

    build_search to model search

--- app/models/concerns/oportunities/scopes.rb
+++ app/models/concerns/oportunities/scopes.rb
@@ -8,5 +8,16 @@ module Oportunities::Scopes
         final: true
       ).order(state: :asc)
     }
+
+    scope :final_without_review, -> {
+      where(final: true).where.not(
+        ConclusionReview.table_name => { review_id: nil }
+      )
+    }
+    scope :not_final_with_review, -> {
+      where(final: false, ConclusionReview.table_name => { review_id: nil })
+    }
+
+    scope :with_or_without_review, -> { not_final_with_review.or(final_without_review) }
   end
 end
