07aa1c29dd83a83248f4f205e0e5e6bdbf606737
Franco Catena <francocatena@gmail.com>
2020-05-08 13:34:19 -0300

    Add weak logic on commitment support

--- app/models/concerns/findings/commitment_support.rb
+++ app/models/concerns/findings/commitment_support.rb
@@ -2,10 +2,17 @@ module Findings::CommitmentSupport
   extend ActiveSupport::Concern
 
   def require_commitment_support? commitment_date
-    FINDING_ANSWER_COMMITMENT_SUPPORT  &&
+    # TODO: remove "weak" logic when customer ask for
+    case FINDING_ANSWER_COMMITMENT_SUPPORT
+    when 'weak'
+      being_implemented?       &&
+      follow_up_date.present?  &&
+      commitment_date.present?
+    when 'true'
       commitment_date.future?          &&
       being_implemented?               &&
       follow_up_date.present?          &&
       follow_up_date < commitment_date
+    end
   end
 end
