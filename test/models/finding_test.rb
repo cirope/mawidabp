993ab966020d8ec0e652a45c679f4ed8209b4995
Franco Catena <francocatena@gmail.com>
2020-05-08 15:20:05 -0300

    Fix finding commitment support test

--- test/models/finding_test.rb
+++ test/models/finding_test.rb
@@ -1407,7 +1407,7 @@ class FindingTest < ActiveSupport::TestCase
   end
 
   test 'require commitment support' do
-    skip unless FINDING_ANSWER_COMMITMENT_SUPPORT
+    skip unless %(true weak).include? FINDING_ANSWER_COMMITMENT_SUPPORT
 
     finding = findings :being_implemented_weakness
 
