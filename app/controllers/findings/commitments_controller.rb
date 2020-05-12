a420b74887c9225164d371f7c93c96c38d04b4ee
Sergio <asergiop21@gmail.com>
2020-05-12 17:13:14 -0300

    Enable limit commitment date for 'NBCH'

--- app/controllers/findings/commitments_controller.rb
+++ app/controllers/findings/commitments_controller.rb
@@ -10,5 +10,14 @@ class Findings::CommitmentsController < ApplicationController
   def show
     @index           = params[:index].to_i
     @commitment_date = Timeliness.parse params[:id], :date
+    @limit_date      = ""
+
+    if defined? ENABLE_LIMIT_COMMITMENT_DATE && ENABLE_LIMIT_COMMITMENT_DATE['limit_date'] == 'true'
+      if @finding.follow_up_date
+        if params[:id].to_date > @finding.follow_up_date + 12.months && @finding.risk == RISK_TYPES[:high]
+          @limit_date = ENABLE_LIMIT_COMMITMENT_DATE["message_one_year"]
+        end
+      end
+    end
   end
 end
