219b04745e7d84582d92236c44d4bef838a602dc
Sergio Palacios <asergiop21@gmail.com>
2020-06-25 21:39:06 -0300

    Now business units are taggagle (#176)

--- app/models/concerns/tags/kinds.rb
+++ app/models/concerns/tags/kinds.rb
@@ -2,6 +2,6 @@ module Tags::Kinds
   extend ActiveSupport::Concern
 
   included do
-    KINDS = %w(control_objective document finding news plan_item review)
+    KINDS = %w(control_objective document finding news plan_item review business_unit)
   end
 end
