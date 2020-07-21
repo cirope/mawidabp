56d1a81e3261f0b04acf71faad348ed2623bc86b
Sergio <asergiop21@gmail.com>
2020-07-21 01:11:09 -0300

    Fix review

--- app/models/file_model_review.rb
+++ app/models/file_model_review.rb
@@ -5,6 +5,6 @@ class FileModelReview < ApplicationRecord
   belongs_to :file_model
   belongs_to :review
 
-  accepts_nested_attributes_for :file_model, :allow_destroy => true,
+  accepts_nested_attributes_for :file_model, allow_destroy: true,
     reject_if: :all_blank
 end
