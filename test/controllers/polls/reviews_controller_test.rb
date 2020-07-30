1464d7cb73200aabdb7425e0acd7f9f9fe2e1826
Sergio <asergiop21@gmail.com>
2020-07-30 18:15:36 -0300

    Add test in review polls

---
+++ test/controllers/polls/reviews_controller_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class Polls::ReviewsControllerTest < ActionController::TestCase
+  setup do
+    @questionnaire = questionnaires :questionnaire_one
+
+    login
+  end
+
+  test 'report reviews' do
+    get :index
+    assert_response :success
+    assert_template 'polls/reviews/index'
+
+    assert_nothing_raised do
+      get :index, params: { index: index_params.merge(answered: 'true') }
+    end
+
+    assert_response :success
+    assert_not_nil assigns(:report)
+    assert_template 'polls/reviews/index'
+  end
+
+  test 'filtered reviews report' do
+    get :index, params: { index: index_params.merge(answered: nil) }
+
+    assert_response :success
+    assert_not_nil assigns(:report)
+    assert_template 'polls/reviews/index'
+  end
+
+  test 'report reviews pdf' do
+    get :index, xhr: true, params: {
+      index: index_params.merge(answered: 'false'),
+      report_title: 'New title',
+      report_subtitle: 'New subtitle'
+    }
+
+    assert_response :success
+    assert_not_nil assigns(:report)
+  end
+
+  private
+
+    def index_params
+      {
+        from_date: 10.years.ago.to_date,
+        to_date: 10.years.from_now.to_date,
+        date_field: %w(created_at issue_date).sample,
+        questionnaire: @questionnaire
+      }
+    end
+end
