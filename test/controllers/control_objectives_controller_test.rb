09c1d3ff3933ad74b7dd3f82c5f1bb2003671d9d
Franco Catena <francocatena@gmail.com>
2020-10-02 18:51:44 -0300

    Update test/controllers/control_objectives_controller_test.rb

--- test/controllers/control_objectives_controller_test.rb
+++ test/controllers/control_objectives_controller_test.rb
@@ -7,6 +7,7 @@ class ControlObjectivesControllerTest < ActionController::TestCase
 
   test 'list control objectives' do
     skip
+
     get :index
     assert_response :success
     assert_not_nil assigns(:control_objectives)
