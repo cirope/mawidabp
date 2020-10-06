b927213fb9901176e4dc712ca34a8e7a89f779e7
Franco Catena <francocatena@gmail.com>
2020-10-06 20:13:02 -0300

    Only way to add control objectives is through PIs

--- app/models/concerns/control_objective_items/destroy_validation.rb
+++ app/models/concerns/control_objective_items/destroy_validation.rb
@@ -6,7 +6,7 @@ module ControlObjectiveItems::DestroyValidation
   end
 
   def can_be_destroyed?
-    !is_in_a_final_review? && weaknesses.empty? && oportunities.empty?
+    false
   end
 
   private
