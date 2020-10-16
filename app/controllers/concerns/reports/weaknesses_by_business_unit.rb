a21e94eb2ad67aecf40a33807013704cff7a46b0
Franco Catena <francocatena@gmail.com>
2020-10-15 22:56:55 -0300

    Repeated findings with no final review on reports

--- app/controllers/concerns/reports/weaknesses_by_business_unit.rb
+++ app/controllers/concerns/reports/weaknesses_by_business_unit.rb
@@ -54,7 +54,7 @@ module Reports::WeaknessesByBusinessUnit
       weaknesses = Weakness.
         with_repeated_status_for_report.
         finals(final).
-        list_with_final_review.
+        list_for_report.
         by_issue_date('BETWEEN', @from_date, @to_date).
         includes(
           :business_unit,
