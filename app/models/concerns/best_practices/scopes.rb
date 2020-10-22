b4aac778badccd7b8346a5b85eb52eab5e52f80f
Sergio <asergiop21@gmail.com>
2020-10-22 00:11:43 -0300

    hide obsolete best practices

--- app/models/concerns/best_practices/scopes.rb
+++ app/models/concerns/best_practices/scopes.rb
@@ -4,4 +4,17 @@ module BestPractices::Scopes
   included do
     scope :ordered, -> { order obsolete: :asc, name: :asc }
   end
+
+  module ClassMethods
+    def visible
+      setting                      = Current.organization.settings.find_by name: 'hide_obsolete_best_practices'
+      hide_obsolete_best_practices = DEFAULT_SETTINGS[:hide_obsolete_best_practices][:value]
+
+      if (setting ? setting.value : hide_obsolete_best_practices) == '0'
+        all
+      else
+        where(obsolete: false)
+      end
+    end
+  end
 end
