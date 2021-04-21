04620bf68116c8d9871bc10ee620241c0eb40bfa
Franco Catena <francocatena@gmail.com>
2021-04-21 11:50:07 -0300

    Customer test deployment config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,    'master'
+set :branch,    'c-patagonia'
 set :stage,     :production
 set :rails_env, 'production'
 
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@app.mawidabp.com}
-role :app, %w{deployer@app.mawidabp.com}
-role :db,  %w{deployer@app.mawidabp.com}
+role :web, %w{deployer@patagonia-gaps.mawidabp.com}
+role :app, %w{deployer@patagonia-gaps.mawidabp.com}
+role :db,  %w{deployer@patagonia-gaps.mawidabp.com}
 
-server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'patagonia-gaps.mawidabp.com', user: 'deployer', roles: %w{web app db}
