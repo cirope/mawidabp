f39b83edcb90d58901b20a344bef80abd0663efe
Franco Catena <francocatena@gmail.com>
2021-04-21 04:00:57 -0300

    Fix opportunities test on scoped cycles

--- test/models/oportunity_test.rb
+++ test/models/oportunity_test.rb
@@ -160,7 +160,14 @@ class OportunityTest < ActiveSupport::TestCase
   end
 
   test 'must be approved on implemented audited' do
-    error_messages = [I18n.t('oportunity.errors.without_solution_date')]
+    error_messages = if USE_SCOPE_CYCLE
+                       [
+                         I18n.t('oportunity.errors.without_solution_date'),
+                         I18n.t('oportunity.errors.without_audit_recommendations')
+                       ]
+                     else
+                       [I18n.t('oportunity.errors.without_solution_date')]
+                     end
 
     @oportunity.state = Finding::STATUS[:implemented_audited]
     @oportunity.solution_date = nil
@@ -170,10 +177,18 @@ class OportunityTest < ActiveSupport::TestCase
   end
 
   test 'must be approved on implemented' do
-    error_messages = [
-      I18n.t('oportunity.errors.with_solution_date'),
-      I18n.t('oportunity.errors.without_follow_up_date')
-    ]
+    error_messages = if USE_SCOPE_CYCLE
+                       [
+                         I18n.t('oportunity.errors.with_solution_date'),
+                         I18n.t('oportunity.errors.without_follow_up_date'),
+                         I18n.t('oportunity.errors.without_audit_recommendations')
+                       ]
+                     else
+                       [
+                         I18n.t('oportunity.errors.with_solution_date'),
+                         I18n.t('oportunity.errors.without_follow_up_date')
+                       ]
+                     end
 
     @oportunity.state = Finding::STATUS[:implemented]
     @oportunity.solution_date = 2.days.from_now.to_date
@@ -184,10 +199,18 @@ class OportunityTest < ActiveSupport::TestCase
   end
 
   test 'must be approved on being implemented' do
-    error_messages = [
-      I18n.t('oportunity.errors.without_answer'),
-      I18n.t('oportunity.errors.without_follow_up_date')
-    ]
+    error_messages = if USE_SCOPE_CYCLE
+                       [
+                         I18n.t('oportunity.errors.without_answer'),
+                         I18n.t('oportunity.errors.without_follow_up_date'),
+                         I18n.t('oportunity.errors.without_audit_recommendations')
+                       ]
+                     else
+                       [
+                         I18n.t('oportunity.errors.without_answer'),
+                         I18n.t('oportunity.errors.without_follow_up_date')
+                       ]
+                     end
 
     @oportunity.state = Finding::STATUS[:being_implemented]
     @oportunity.answer = ' '
@@ -197,7 +220,14 @@ class OportunityTest < ActiveSupport::TestCase
   end
 
   test 'must be approved invalid state' do
-    error_messages = [I18n.t('oportunity.errors.not_valid_state')]
+    error_messages = if USE_SCOPE_CYCLE
+                       [
+                         I18n.t('oportunity.errors.not_valid_state'),
+                         I18n.t('oportunity.errors.without_audit_recommendations')
+                       ]
+                     else
+                       [I18n.t('oportunity.errors.not_valid_state')]
+                     end
 
     @oportunity.state = Finding::STATUS[:notify]
 
@@ -206,7 +236,14 @@ class OportunityTest < ActiveSupport::TestCase
   end
 
   test 'must be approved on users' do
-    error_messages = [I18n.t('oportunity.errors.without_audited')]
+    error_messages = if USE_SCOPE_CYCLE
+                       [
+                         I18n.t('oportunity.errors.without_audited'),
+                         I18n.t('oportunity.errors.without_audit_recommendations')
+                       ]
+                     else
+                       [I18n.t('oportunity.errors.without_audited')]
+                     end
 
     @oportunity.state = Finding::STATUS[:assumed_risk]
     @oportunity.finding_user_assignments =
@@ -226,7 +263,14 @@ class OportunityTest < ActiveSupport::TestCase
   end
 
   test 'must be approved on required attributes' do
-    error_messages = [I18n.t('oportunity.errors.without_audit_comments')]
+    error_messages = if USE_SCOPE_CYCLE
+                       [
+                         I18n.t('oportunity.errors.without_audit_comments'),
+                         I18n.t('oportunity.errors.without_audit_recommendations')
+                       ]
+                     else
+                       [I18n.t('oportunity.errors.without_audit_comments')]
+                     end
 
     @oportunity.state = Finding::STATUS[:assumed_risk]
     @oportunity.audit_comments = '  '
@@ -240,7 +284,14 @@ class OportunityTest < ActiveSupport::TestCase
   end
 
   test 'must be approved on tasks' do
-    error_messages = [I18n.t('oportunity.errors.with_expired_tasks')]
+    error_messages = if USE_SCOPE_CYCLE
+                       [
+                         I18n.t('oportunity.errors.with_expired_tasks'),
+                         I18n.t('oportunity.errors.without_audit_recommendations')
+                       ]
+                     else
+                       [I18n.t('oportunity.errors.with_expired_tasks')]
+                     end
 
     @oportunity.state = Finding::STATUS[:assumed_risk]
 
