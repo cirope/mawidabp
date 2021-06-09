705b37bc4c9b87f26aa96ec81941669896212b1c
Sergio <asergiop21@gmail.com>
2021-06-09 11:32:52 -0300

    effects and compliance_test validations in best practices

--- app/models/concerns/control_objectives/control.rb
+++ app/models/concerns/control_objectives/control.rb
@@ -2,10 +2,22 @@ module ControlObjectives::Control
   extend ActiveSupport::Concern
 
   included do
+    before_validation :enable_control_validations
+
     has_one :control, -> {
       order Arel.sql("#{Control.quoted_table_name}.#{Control.qcn('order')} ASC")
     }, as: :controllable, dependent: :destroy
 
     accepts_nested_attributes_for :control, allow_destroy: true
   end
+
+  private
+
+    def enable_control_validations
+      if !HIDE_CONTROL_OBJECTIVE_ITEM_EFFECTIVENESS &&
+          HIDE_FINDING_CRITERIA_MISMATCH
+        control.validates_presence_of_effects = true
+        control.validates_presence_of_compliance_tests = true
+      end
+    end
 end
