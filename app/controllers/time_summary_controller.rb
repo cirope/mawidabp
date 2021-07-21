ef9c1338eb3f4e15db84c7bede7a81cfbef14a59
Sergio <asergiop21@gmail.com>
2021-07-21 16:00:18 -0300

    Fix review

--- app/controllers/time_summary_controller.rb
+++ app/controllers/time_summary_controller.rb
@@ -51,7 +51,7 @@ class TimeSummaryController < ApplicationController
   end
 
   def show
-    review = Review.find params[:id]
+    review = Review.list.find params[:id]
 
     @amounts = {
       'workflow':         review.workflow.try(:human_units).to_f,
