60174490a9af7993af5968b3216bc2a28f0864d2
seba-hullop <sebastian@hullop.com>
2021-07-29 15:04:29 -0300

    agrego migracion

---
+++ db/migrate/20210729175353_add_auxiliar_business_unit.rb
@@ -0,0 +1,9 @@
+class AddAuxiliarBusinessUnit < ActiveRecord::Migration[6.0]
+  def change
+    create_table :appointments do |t|
+      t.belongs_to :plan_item, index: true
+      t.belongs_to :business_unit, index: true
+      t.timestamps
+    end
+  end
+end
