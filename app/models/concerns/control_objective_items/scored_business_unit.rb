2c05f027d5d98e25f8313c5f6485c8235366f17e
seba-hullop <sebastian@hullop.com>
2021-08-02 11:33:06 -0300

    agrego vista para seleccionar unidad de negocio evaluada

--- app/models/concerns/control_objective_items/scored_business_unit.rb
+++ app/models/concerns/control_objective_items/scored_business_unit.rb
@@ -4,6 +4,6 @@ module ControlObjectiveItems::ScoredBusinessUnit
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :scored_business_unit, class_name: 'BusinessUnit'
+    belongs_to :scored_business_unit, class_name: 'BusinessUnit', optional: true
   end
 end
