1bd6161977cca0d7e37a0bf57d177c620ce017b0
Sergio <asergiop21@gmail.com>
2021-08-23 13:38:50 -0300

    Fix review

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
-    es2021: true
   },
   parserOptions: {
-    ecmaVersion: 12
+    ecmaversion: 5,
+    sourceType: 'script'
   },
   rules: {
     'array-bracket-spacing': ['error', 'never'],
     'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
-    eqeqeq: ['error', 'always'],
-    indent: ['error', 2],
+    'eqeqeq': ['error', 'always'],
+    'indent': ['error', 2],
     'keyword-spacing': ['error', { 'before': true, 'after': true }],
     'linebreak-style': ['error', 'unix'],
     'no-console': 'warn',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
     'space-before-blocks': ['error', 'always'],
     'space-before-function-paren': ['error', 'always'],
     'space-infix-ops': 'error',
