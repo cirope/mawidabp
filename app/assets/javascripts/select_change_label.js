10948d0ead99ee4b04a6d1749289a9f6d40356f6
Franco Catena <francocatena@gmail.com>
2021-09-24 13:42:27 -0300

    Update app/assets/javascripts/select_change_label.js

--- app/assets/javascripts/select_change_label.js
+++ app/assets/javascripts/select_change_label.js
@@ -12,6 +12,7 @@ jQuery(function () {
   function changeLabel(element) {
     var idLabel = element.data('formClass').toLowerCase().concat('_', element.data('targetLabel'))
     var $labelTarget = $('label[for="' + idLabel + '"]')
+
     if (element.find(':selected').val() == element.data('targetValue')) {  
       var text = $labelTarget.text().concat(element.data('suffix'))
     } else {
