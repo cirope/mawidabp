4dd4b91082d2178bdb09d22c625901f8413dc801
Sergio <asergiop21@gmail.com>
2021-10-01 18:20:09 -0300

    Fix review

--- test/models/ldap_config_test.rb
+++ test/models/ldap_config_test.rb
@@ -181,15 +181,12 @@ class LdapConfigTest < ActiveSupport::TestCase
     assert user.organization_roles.map(&:role_id).exclude?(role.id)
     assert user.organization_roles.map(&:role_id).exclude?(corp_role.id)
 
-    @ldap_config.organizational_unit = 'dn'
-
     assert_difference 'User.count' do
       @ldap_config.import 'admin', 'admin123'
     end
 
     assert user.reload.organization_roles.map(&:role_id).include?(role.id)
     assert user.organization_roles.map(&:role_id).include?(corp_role.id)
-    assert user.organizational_unit.present?
     assert_equal user.id, User.find_by(user: 'new_user').manager_id
     assert_nil user.manager_id
   end
