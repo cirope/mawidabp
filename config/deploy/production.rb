cb13dc83d34ea67e451382ebf22305b853967596
Sergio <asergiop21@gmail.com>
2021-10-01 00:48:29 -0300

    Rename branch and url on deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,    'master'
+set :branch,    'c-dino'
 set :stage,     :production
 set :rails_env, 'production'
 
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@app.mawidabp.com}
-role :app, %w{deployer@app.mawidabp.com}
-role :db,  %w{deployer@app.mawidabp.com}
+role :web, %w{deployer@dino.mawidabp.com}
+role :app, %w{deployer@dino.mawidabp.com}
+role :db,  %w{deployer@dino.mawidabp.com}
 
-server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'dino.mawidabp.com', user: 'deployer', roles: %w{web app db}
