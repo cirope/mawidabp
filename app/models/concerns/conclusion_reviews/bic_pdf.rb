4baeedf4bc49e333a94f95596809db67b037aac7
Sergio Palacios <asergiop21@gmail.com>
2021-12-16 15:34:44 -0300

    Update app/models/concerns/conclusion_reviews/bic_pdf.rb
    Co-authored-by: Franco Catena <francocatena@gmail.com>

--- app/models/concerns/conclusion_reviews/bic_pdf.rb
+++ app/models/concerns/conclusion_reviews/bic_pdf.rb
@@ -246,9 +246,8 @@ module ConclusionReviews::BicPdf
                    end
 
       weaknesses = bic_exclude_regularized_findings weaknesses
-
-      present  = weaknesses.not_revoked.where repeated_of_id: nil
-      repeated = weaknesses.not_revoked.where.not repeated_of_id: nil
+      present    = weaknesses.not_revoked.where repeated_of_id: nil
+      repeated   = weaknesses.not_revoked.where.not repeated_of_id: nil
 
       present.reorder(risk: :desc, priority: :desc, review_code: :asc).each do |weakness|
         put_bic_image_on pdf, weakness, number += 1
