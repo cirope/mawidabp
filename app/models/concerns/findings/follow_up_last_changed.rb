f330a58825e6234c50304b3774c7ef284e25aadb
seba-hullop <sebastian@hullop.com>
2021-12-29 12:33:45 -0300

    cambio nombre de variable

--- app/models/concerns/findings/follow_up_last_changed.rb
+++ app/models/concerns/findings/follow_up_last_changed.rb
@@ -12,16 +12,16 @@ module Findings::FollowUpLastChanged
   end
 
   def follow_up_date_last_changed_on_versions
-    current = self
+    current_finding = self
 
     versions.map(&:reify).reject(&:blank?).reverse_each do |reify|
-      if current.follow_up_date != reify.follow_up_date
-        return I18n.l(current.updated_at, format: :minimal)
+      if current_finding.follow_up_date != reify.follow_up_date
+        return I18n.l(current_finding.updated_at, format: :minimal)
       end
 
-      current = reify
+      current_finding = reify
     end
 
-    current.follow_up_date.present? ? I18n.l(current.updated_at, format: :minimal) : nil
+    current_finding.follow_up_date.present? ? I18n.l(current_finding.updated_at, format: :minimal) : nil
   end
 end
