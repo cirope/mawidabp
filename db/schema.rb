d6df9e6dae0b59c79c5b91668668d722e2b82ac9
seba-hullop <sebastian@hullop.com>
2022-02-10 19:40:17 -0300

    primer version sin tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_142340) do
+ActiveRecord::Schema.define(version: 2022_02_10_213014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -186,6 +186,9 @@ ActiveRecord::Schema.define(version: 2021_12_22_142340) do
     t.boolean "hide_review_logo", default: false, null: false
     t.boolean "independent_identification", default: false, null: false
     t.boolean "shared_business_units", default: false, null: false
+    t.boolean "without_number", default: false, null: false
+    t.string "reviews_for"
+    t.string "detailed_review"
     t.index ["external"], name: "index_business_unit_types_on_external"
     t.index ["name"], name: "index_business_unit_types_on_name"
     t.index ["organization_id"], name: "index_business_unit_types_on_organization_id"
