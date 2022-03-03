70cc289da25a77fa7d27f4edc4428c1505f83f15
seba-hullop <sebastian@hullop.com>
2022-03-03 11:23:51 -0300

    correcciones

--- app/assets/javascripts/select_change_label.js
+++ app/assets/javascripts/select_change_label.js
@@ -15,30 +15,25 @@ jQuery(function () {
     var risk                       = $(element.data('targetInputWithRisk')).val()
     var state                      = $(element.data('targetInputWithState')).val()
     var values_states_change_label = element.data('targetValuesStatesChangeLabel')
+    var text                       = $labelTarget.text().split(element.data('suffix'))[0]
 
-    if ((origination_date != null) && (risk != '') && (values_states_change_label.includes(parseInt(state)))){
+    if ((origination_date != null) && (risk != '') && (values_states_change_label.includes(parseInt(state)))) {
       var days_to_add    = (element.data('daysToAdd'))[parseInt(risk)]
       var suggested_date = new Date()
 
       suggested_date.setDate(origination_date.getDate() + days_to_add)
 
-      var text = $labelTarget.text().concat(element.data('suffix')).concat(formatDate(suggested_date))
-    } else {
-      var text = $labelTarget.text().split(element.data('suffix'))[0]
+      var text = text.concat(element.data('suffix')).concat(formatDate(suggested_date))
     }
 
     $($labelTarget).text(text)
   }
 
-  function formatDate(date){
-    let day = date.getDate()
-    let month = date.getMonth() + 1
-    let year = date.getFullYear()
+  function formatDate(date) {
+    let day   = String(date.getDate()).padStart(2,'0')
+    let month = String(date.getMonth() + 1).padStart(2,'0')
+    let year  = date.getFullYear()
 
-    if(month < 10){
-      return day + '/0' + month + '/' + year
-    } else {
-      return day + '/' + month + '/' + year
-    }
+    return day + '/' + month + '/' + year
   }
 })
--- app/helpers/findings_helper.rb
+++ app/helpers/findings_helper.rb
@@ -469,14 +469,18 @@ module FindingsHelper
     end
 
     def data_options_for_suggested_follow_up_date
-      {
-        target_input_with_origination_date: '#weakness_origination_date',
-        target_input_with_risk: '#weakness_risk',
-        target_input_with_state: '#weakness_state',
-        target_values_states_change_label: states_that_suggest_follow_up_date,
-        days_to_add: suggestion_to_add_days_follow_up_date_depending_on_the_risk,
-        suffix: I18n.t('findings.weakness.follow_up_date_label_append'),
-        target_input_with_label: '#weakness_follow_up_date'
-      }
+      if USE_SCOPE_CYCLE
+        {
+          target_input_with_origination_date: '#weakness_origination_date',
+          target_input_with_risk: '#weakness_risk',
+          target_input_with_state: '#weakness_state',
+          target_values_states_change_label: states_that_suggest_follow_up_date,
+          days_to_add: suggestion_to_add_days_follow_up_date_depending_on_the_risk,
+          suffix: I18n.t('findings.weakness.follow_up_date_label_append'),
+          target_input_with_label: '#weakness_follow_up_date'
+        }
+      else
+        {}
+      end
     end
 end
