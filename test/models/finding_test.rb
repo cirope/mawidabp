966106b8fe2dc85e87d98944d952d67197a758df
Ismael Isuani <ismaelisuani@gmail.com>
2022-04-04 08:49:14 -0300

    Improve finding test

--- test/models/finding_test.rb
+++ test/models/finding_test.rb
@@ -1439,7 +1439,7 @@ class FindingTest < ActiveSupport::TestCase
   end
 
   test 'require commitment support' do
-    skip unless %w(true).include? FINDING_ANSWER_COMMITMENT_SUPPORT
+    skip unless %(true).include? FINDING_ANSWER_COMMITMENT_SUPPORT
 
     finding = findings :being_implemented_weakness
 
