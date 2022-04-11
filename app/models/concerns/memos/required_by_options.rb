c790327f265af6b7974311f698945e78ce082d54
seba-hullop <sebastian@hullop.com>
2022-04-11 15:48:32 -0300

    cambio opciones de required_by

--- app/models/concerns/memos/required_by_options.rb
+++ app/models/concerns/memos/required_by_options.rb
@@ -2,6 +2,11 @@ module Memos::RequiredByOptions
   extend ActiveSupport::Concern
 
   included do
-    REQUIRED_BY_OPTIONS = %w[AP AF AC AS]
+    REQUIRED_BY_OPTIONS = [
+      'Sindicatura General de la Nación',
+      'Auditoria General de la Nación',
+      'Banco Central de la República Argentina',
+      'Auditor Externo'
+    ]
   end
 end
