2a27fcbae9067c72e0c16c24dc819be6516142a0
seba-hullop <85506776+seba-hullop@users.noreply.github.com>
2022-06-21 15:54:45 -0300

    Add script to run the test with different configurations (#443)

---
+++ execute_all_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+array=( bh bic cycle_score gal normal reviews_scored_by_weakness )
+
+for i in "${array[@]}"
+do
+  echo "ejecutando tests con application.$i.yml"
+  CONFIG_TYPE=$i rails test
+done
