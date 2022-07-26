db0751ab056abf5c63aa87f29aa5beb4f62a7737
seba-hullop <85506776+seba-hullop@users.noreply.github.com>
2022-07-26 12:22:50 -0300

    MGraph email _adapter_ (#470)

--- app/models/concerns/emails/fetch.rb
+++ app/models/concerns/emails/fetch.rb
@@ -3,42 +3,25 @@ module Emails::Fetch
 
   module ClassMethods
     def fetch
-      if email_method?
-        config
-
-        Mail.all.each do |mail|
-          begin
-            Finding.receive_mail mail
-          rescue Exception => exception
-            log exception, mail
-          end
-        end
-      end
+      reschedule_strategy.fetch if email_method?
     end
 
     def email_method?
       ENV['EMAIL_METHOD']
     end
 
+    def clean_answer mail
+      reschedule_strategy.clean_answer mail
+    end
+
     private
 
-      def config
-        Mail.defaults do
-          email_method = ENV['EMAIL_METHOD'].to_sym
-
-          retriever_method email_method, address:    ENV['EMAIL_SERVER'],
-                                         port:       ENV['EMAIL_PORT'],
-                                         user_name:  ENV['EMAIL_USER_NAME'],
-                                         password:   ENV['EMAIL_PASSWORD'],
-                                         enable_ssl: ENV['EMAIL_SSL'] != 'false'
+      def reschedule_strategy
+        if %w[pop3 imap].include? ENV['EMAIL_METHOD']
+          EmailReceiverStrategies::ImapPopStrategy.new
+        elsif %w[mgraph].include? ENV['EMAIL_METHOD']
+          EmailReceiverStrategies::MGraphStrategy.new
         end
-      end
-
-      def log exception, mail
-        logger = Logger.new 'log/mailman.log'
-
-        logger.error "Exception occurred while receiving message:\n#{mail}"
-        logger.error [exception, *exception.backtrace].join("\n")
       end
   end
 end
