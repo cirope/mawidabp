4aac772b67e4263ebfc9ef4c5a9e9fc2f090e7cc
seba-hullop <sebastian@hullop.com>
2022-09-06 17:21:24 -0300

    correcciones en js [skip actions]

--- app/assets/javascripts/enable_disable_card_fieldset.js
+++ app/assets/javascripts/enable_disable_card_fieldset.js
@@ -1,13 +1,13 @@
 jQuery(function ($) {
   $(document).on('shown.bs.collapse', '[data-enable-disable-card-fieldset]', function (event) {
-    if($(this).data('enableDisableCardFieldset') == true){
+    if ($(this).data('enableDisableCardFieldset')) {
       $(this).find('fieldset').prop('disabled', false)
       $(this).find('fieldset :input:first').focus()
     }
   })
 
   $(document).on('hidden.bs.collapse', '[data-enable-disable-card-fieldset]', function (event) {
-    if($(this).data('enableDisableCardFieldset') == true){
+    if ($(this).data('enableDisableCardFieldset')) {
       $(this).find('fieldset').prop('disabled', true)
     }
   })
