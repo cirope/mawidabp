eb21c0282f49d2532222180ed016ef24cbe5f5bc
Sergio <asergiop21@gmail.com>
2022-09-14 09:21:26 -0300

    Downgrade migrate version in add_attached_to_answers

--- db/migrate/20220905172357_add_attached_to_answers.rb
+++ db/migrate/20220905172357_add_attached_to_answers.rb
@@ -1,4 +1,4 @@
-class AddAttachedToAnswers < ActiveRecord::Migration[6.1]
+class AddAttachedToAnswers < ActiveRecord::Migration[6.0]
   def change
     add_column :answers, :attached, :string
   end
