28797339a4e103eb3f8ba63c278832077d8696de
Sergio <asergiop21@gmail.com>
2022-10-05 20:04:43 -0300

    Setting comercial deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,    'master'
+set :branch,    'c-comercial'
 set :stage,     :production
 set :rails_env, 'production'
 
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@app.mawidabp.com}
-role :app, %w{deployer@app.mawidabp.com}
-role :db,  %w{deployer@app.mawidabp.com}
+role :web, %w{deployer@comercial.mawidabp.com}
+role :app, %w{deployer@comercial.mawidabp.com}
+role :db,  %w{deployer@comercial.mawidabp.com}
 
-server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'comercial.mawidabp.com', user: 'deployer', roles: %w{web app db}
