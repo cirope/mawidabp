682081941452a9f0add475cd9be9f18c2c5d6e3e
Sergio <asergiop21@gmail.com>
2022-10-24 18:07:31 -0300

    Setting deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :linked_files, %w{config/application.yml}
 set :linked_dirs, %w{log private tmp/pids}
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.7.5'
+set :rbenv_ruby, '2.7.6'
 
 set :keep_releases, 5
 
