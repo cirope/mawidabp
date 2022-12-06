99cf533364d8f6248acff1dac89cd09c66c15416
Jonathan Villegas <villegas.jonathan88@gmail.com>
2022-12-06 08:16:37 -0300

    Adds not applicable option to reports without control objectives for scoring

--- app/helpers/conclusion_draft_reviews_helper.rb
+++ app/helpers/conclusion_draft_reviews_helper.rb
@@ -54,4 +54,20 @@ module ConclusionDraftReviewsHelper
 
     result.join "\r\n\r\n"
   end
+
+  def score_text_for conclusion_review
+    if Current.conclusion_pdf_format == 'pat' && conclusion_review.review.control_objective_items_for_score.present?
+      conclusion_review.review.score_text
+    else
+      I18n.t 'score_types.none'
+    end
+  end
+
+  def score_alt_text_for conclusion_review
+    if Current.conclusion_pdf_format == 'pat' && conclusion_review.review.control_objective_items_for_score.present?
+      conclusion_review.review.score_alt_text
+    else
+      I18n.t 'score_types.none'
+    end
+  end
 end
