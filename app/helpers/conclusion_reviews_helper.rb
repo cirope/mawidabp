027bdff369f6a7f768b5ce00a1ff67a0a74e2d75
Jonathan Villegas <villegas.jonathan88@gmail.com>
2022-12-06 17:20:14 -0300

    Remove USE_SCOPE_CYCLE condition

--- app/helpers/conclusion_reviews_helper.rb
+++ app/helpers/conclusion_reviews_helper.rb
@@ -1,6 +1,6 @@
 module ConclusionReviewsHelper
   def score_text_for conclusion_review
-    if USE_SCOPE_CYCLE && conclusion_review.review.control_objective_items_for_score.present?
+    if conclusion_review.review.control_objective_items_for_score.present?
       conclusion_review.review.score_text
     else
       I18n.t 'score_types.none'
