e58125d944027db2d64b4076a8264ebd72381c2a
Jonathan Villegas <villegas.jonathan88@gmail.com>
2022-12-06 08:58:27 -0300

    Modify patagonia variable

--- app/helpers/conclusion_draft_reviews_helper.rb
+++ app/helpers/conclusion_draft_reviews_helper.rb
@@ -56,7 +56,7 @@ module ConclusionDraftReviewsHelper
   end
 
   def score_text_for conclusion_review
-    if Current.conclusion_pdf_format == 'pat' && conclusion_review.review.control_objective_items_for_score.present?
+    if USE_SCOPE_CYCLE && conclusion_review.review.control_objective_items_for_score.present?
       conclusion_review.review.score_text
     else
       I18n.t 'score_types.none'
@@ -64,7 +64,7 @@ module ConclusionDraftReviewsHelper
   end
 
   def score_alt_text_for conclusion_review
-    if Current.conclusion_pdf_format == 'pat' && conclusion_review.review.control_objective_items_for_score.present?
+    if USE_SCOPE_CYCLE && conclusion_review.review.control_objective_items_for_score.present?
       conclusion_review.review.score_alt_text
     else
       I18n.t 'score_types.none'
