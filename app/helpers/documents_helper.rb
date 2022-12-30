f4195dd225c3fd99c455e0b90acb18fa93d2789a
seba-hullop <sebastian@hullop.com>
2022-12-30 12:51:25 -0300

    saco metodos y helpers con file_models

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,22 +1,4 @@
 module DocumentsHelper
-  def link_to_download_document document, options = {}
-    url   = url_from_document_file_model(document) || url_from_document_description(document)
-    title = document.file_model&.identifier&.titleize || url
-    icon  = document.file_model ? 'download' : 'link'
-
-    if url.present?
-      default_options = {
-        class: 'btn btn-outline-secondary',
-        title: title,
-        data:  { ignore_unsaved_data: true }
-      }
-
-      link_to url, default_options.merge(options) do
-        icon 'fas', icon
-      end
-    end
-  end
-
   def show_document_info document
     created_at_label = Document.human_attribute_name 'created_at'
     updated_at_label = Document.human_attribute_name 'updated_at'
@@ -30,14 +12,6 @@ module DocumentsHelper
   end
 
   private
-
-    def url_from_document_file_model document
-      file_model = document.file_model
-
-      if file_model && file_model.file? && file_model.file.cached?.blank?
-        download_document_path document
-      end
-    end
 
     def url_from_document_description document
       url_regex = /(ftp|file|http|https):\/\/[\\\w\-.:%]+(\/\S*)?/
