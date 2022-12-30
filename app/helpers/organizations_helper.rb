d688b17067ffdf8c03496ac98a4d4d14318ede69
seba-hullop <sebastian@hullop.com>
2022-12-30 12:55:29 -0300

    saco helpers que utilizan file_model

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,33 +1,7 @@
 module OrganizationsHelper
-  def organization_image_tag thumb_name: :thumb, model: :image_model
-    scoped_organization_image model, thumb_name if image_persisted? model
-  end
-
-  def organization_image model = :image_model
-    @organization.send "build_#{model}" unless @organization.send model
-
-    @organization.send model
-  end
-
   def organization_logo_style_options
     %w(default success info warning danger).map do |style|
       [t("organizations.logo_styles.#{style}"), style]
     end
   end
-
-  private
-
-    def image_persisted? model
-      @organization.send(model) && !@organization.send(model).image_cache
-    end
-
-    def scoped_organization_image model, thumb_name
-      Fiber.new do
-        Current.organization = @organization
-
-        image_tag @organization.send(model).image.url(thumb_name),
-          size: @organization.send(model).image_size(thumb_name),
-          alt: 'Logo'
-      end.resume
-    end
 end
