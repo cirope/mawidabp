693003b6e8d6bf00e908fec301ab218c2cb6d2d9
Jonathan Villegas <villegas.jonathan88@gmail.com>
2023-01-07 12:41:55 -0300

    Add ExternalReviews data

--- app/models/concerns/reviews/pdf.rb
+++ app/models/concerns/reviews/pdf.rb
@@ -8,14 +8,16 @@ module Reviews::Pdf
     pdf.add_title *pdf_title
     pdf.move_down PDF_FONT_SIZE
 
-    pdf.add_description_item Review.human_attribute_name('identification'),
-      identification, 0, false, PDF_FONT_SIZE
-    pdf.add_description_item Review.human_attribute_name('plan_item'),
-      plan_item.project, 0, false, PDF_FONT_SIZE
-
-    pdf.move_down PDF_FONT_SIZE
-
+    put_description pdf
     put_control_objective_items_table_on pdf
+
+    if Current.conclusion_pdf_format == 'nbc'
+      external_reviews.map(&:alternative_review).each do |alt_review|
+        pdf.start_new_page
+        alt_review.put_description pdf
+        alt_review.put_control_objective_items_table_on pdf
+      end
+    end
 
     pdf.custom_save_as pdf_name, Review.table_name, id
   end
@@ -33,14 +35,15 @@ module Reviews::Pdf
       identification: identification.sanitized_for_filename
   end
 
-  private
+  protected
 
-    def pdf_title
-      [
-        [Review.model_name.human, identification].join(' '),
-        (PDF_FONT_SIZE * 1.5).round,
-        :center
-      ]
+    def put_description pdf
+      pdf.add_description_item Review.human_attribute_name('identification'),
+        identification, 0, false, PDF_FONT_SIZE
+      pdf.add_description_item Review.human_attribute_name('plan_item'),
+        plan_item.project, 0, false, PDF_FONT_SIZE
+
+      pdf.move_down PDF_FONT_SIZE
     end
 
     def put_control_objective_items_table_on pdf
@@ -57,6 +60,16 @@ module Reviews::Pdf
           )
         end
       end
+    end
+
+  private
+
+    def pdf_title
+      [
+        [Review.model_name.human, identification].join(' '),
+        (PDF_FONT_SIZE * 1.5).round,
+        :center
+      ]
     end
 
     def pdf_columns
