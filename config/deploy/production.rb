d166299295c42a60b69255a45b93c7a130f51d7e
Sergio <asergiop21@gmail.com>
2023-01-31 17:47:18 -0300

    Setting deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,    'master'
+set :branch,    'c-mariva-production'
 set :stage,     :production
 set :rails_env, 'production'
 
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@app.mawidabp.com}
-role :app, %w{deployer@app.mawidabp.com}
-role :db,  %w{deployer@app.mawidabp.com}
+role :web, %w{deployer@mariva.mawidabp.com}
+role :app, %w{deployer@mariva.mawidabp.com}
+role :db,  %w{deployer@mariva.mawidabp.com}
 
-server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'mariva.mawidabp.com', user: 'deployer', roles: %w{web app db}
