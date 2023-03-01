05377ead1977e1b2b95b2b8bd7c05bb5fc337099
Sergio <asergiop21@gmail.com>
2023-03-01 13:53:23 -0300

    Setting production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@banco-hipotecario-testing.mawidabp.com}
-role :app, %w{deployer@banco-hipotecario-testing.mawidabp.com}
-role :db,  %w{deployer@banco-hitotecario-testing.mawidabp.com}
+role :web, %w{deployer@hipotecario-testing.mawidabp.com}
+role :app, %w{deployer@hipotecario-testing.mawidabp.com}
+role :db,  %w{deployer@hitotecario-testing.mawidabp.com}
 
-server 'banco-hipotecario-testing.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'hipotecario-testing.mawidabp.com', user: 'deployer', roles: %w{web app db}
