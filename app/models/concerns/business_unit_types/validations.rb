f2fae20e2bd03d10326129e6707b980a8a57e836
Jonathan Villegas <villegas.jonathan88@gmail.com>
2023-03-31 04:35:35 -0300

    Add exec_summary_intro_must_have_valid_keys validation

--- app/models/concerns/business_unit_types/validations.rb
+++ app/models/concerns/business_unit_types/validations.rb
@@ -12,9 +12,25 @@ module BusinessUnitTypes::Validations
       case_sensitive: false, scope: :organization_id
     }
     validate :all_units_marked_for_destruction_can_be_destroyed
+    validate :exec_summary_intro_must_have_valid_keys, if: :is_gal?
   end
 
   private
+
+    def is_gal?
+      Current.conclusion_pdf_format == 'gal'
+    end
+
+    def exec_summary_intro_must_have_valid_keys
+      field_keys   = exec_summary_intro.scan(/%\{(.*?)\}/).flatten
+      valid_keys   = ['informe']
+      missing_keys = field_keys - valid_keys
+
+      if missing_keys.any?
+        errors.add :exec_summary_intro, :missing_keys, count: missing_keys.count,
+          valid_keys: valid_keys.to_sentence, invalid_keys: missing_keys.to_sentence
+      end
+    end
 
     def all_units_marked_for_destruction_can_be_destroyed
       locked = business_units.any? do |bu|
