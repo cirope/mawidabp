379543f8a4a396b6bf14fcf05b1f76c0a3342cf7
Sergio <asergiop21@gmail.com>
2023-05-02 09:46:37 -0300

    Setting deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,    'master'
+set :branch,    'c-smg-test'
 set :stage,     :production
 set :rails_env, 'production'
 
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@app.mawidabp.com}
-role :app, %w{deployer@app.mawidabp.com}
-role :db,  %w{deployer@app.mawidabp.com}
+role :web, %w{deployer@smg-test.mawidabp.com}
+role :app, %w{deployer@smg-test.mawidabp.com}
+role :db,  %w{deployer@smg-test.mawidabp.com}
 
-server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'smg-test.mawidabp.com', user: 'deployer', roles: %w{web app db}
