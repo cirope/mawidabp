72546f2bba45443cf47bf74490190db744b27c6a
Jonathan Villegas <villegas.jonathan88@gmail.com>
2023-05-19 04:51:33 -0300

    Add validations

--- app/forms/nbc_internal_control_qualification_as_group_of_companies_form.rb
+++ app/forms/nbc_internal_control_qualification_as_group_of_companies_form.rb
@@ -7,6 +7,8 @@ class NbcInternalControlQualificationAsGroupOfCompaniesForm < NbcAnnualReportFor
 
   validates :previous_period_id, presence: true
   validate :periods_not_equal
+  validate :organization_units_must_have_same_name
+  validate :periods_must_be_the_same
 
   def business_unit_type
     BusinessUnitType.list.find business_unit_type_id
@@ -24,4 +26,40 @@ class NbcInternalControlQualificationAsGroupOfCompaniesForm < NbcAnnualReportFor
         errors.add :previous_period_id, :must_be_different
       end
     end
+
+    def organization_units_must_have_same_name
+      byebug
+      organization_ids = Organization.where(prefix: ORGANIZATIONS_WITH_INTERNAL_CONTROL_QUALIFICATION_REPORT).pluck(:id)
+
+      business_unit_types_per_organization = BusinessUnitType
+        .where(organization_id: organization_ids)
+        .group(:organization_id)
+        .pluck(:organization_id, Arel.sql("array_agg(name ORDER BY name)"))
+        .to_h
+
+      first_organization = business_unit_types_per_organization.values.first
+
+      unless business_unit_types_per_organization.values.all? { |names| names == first_organization } &&
+        business_unit_types_per_organization.keys.length == organization_ids.length
+        errors.add :organizations, :must_have_the_same_business_unit_types
+      end
+    end
+
+    def periods_must_be_the_same
+      organization_ids = Organization.where(prefix: ORGANIZATIONS_WITH_INTERNAL_CONTROL_QUALIFICATION_REPORT).pluck(:id)
+
+      periods_per_organization = Period
+        .where(organization_id: organization_ids)
+        .group(:organization_id)
+        .pluck(:organization_id, Arel.sql("array_agg(name ORDER BY name)"))
+        .to_h
+
+      first_period = periods_per_organization.values.first
+
+      unless periods_per_organization.values.all? { |names| names == first_period } &&
+        periods_per_organization.keys.length == organization_ids.length
+        errors.add :periods, :must_be_the_same
+      end
+    end
+
 end
