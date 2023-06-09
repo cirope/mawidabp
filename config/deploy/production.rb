6930c43e5fe51025bc0181c9161d2eede9c4035f
Sergio <asergiop21@gmail.com>
2023-06-09 18:41:04 -0300

    Setting deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@app.mawidabp.com}
-role :app, %w{deployer@app.mawidabp.com}
-role :db,  %w{deployer@app.mawidabp.com}
+role :web, %w{deployer@comafi.mawidabp.com}
+role :app, %w{deployer@comafi.mawidabp.com}
+role :db,  %w{deployer@comafi.mawidabp.com}
 
-server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'comafi.mawidabp.com', user: 'deployer', roles: %w{web app db}
