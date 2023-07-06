4ca20efc728705b7ebb3c054b86c8756a45ac06e
Sergio Palacios <asergiop21@gmail.com>
2023-07-06 18:52:13 -0300

    Modify default sync ldap value in organization roles (#574)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_29_201731) do
+ActiveRecord::Schema.define(version: 2023_07_06_204023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -817,7 +817,7 @@ ActiveRecord::Schema.define(version: 2023_06_29_201731) do
     t.integer "role_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "sync_ldap", default: true, null: false
+    t.boolean "sync_ldap", null: false
     t.index ["organization_id"], name: "index_organization_roles_on_organization_id"
     t.index ["role_id"], name: "index_organization_roles_on_role_id"
     t.index ["user_id"], name: "index_organization_roles_on_user_id"
