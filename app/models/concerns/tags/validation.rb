672e946024c02c2f461849bc14ccc7c59241008b
Sergio <asergiop21@gmail.com>
2023-10-10 18:55:20 -0300

    validate tag by organization

--- app/models/concerns/tags/validation.rb
+++ app/models/concerns/tags/validation.rb
@@ -3,16 +3,32 @@ module Tags::Validation
 
   included do
     validates :name, :kind, :style, :icon, presence: true, length: { maximum: 255 }
-    validates :name, uniqueness: { case_sensitive: false, scope: :group_id }
     validates :icon, inclusion: { in: :available_icons }
     validates :kind, inclusion: { in: Tag::KINDS }
     validates :style, inclusion: {
       in: %w(secondary primary success info warning danger)
     }
+    validate :tag_uniqueness
     validate :shared_reversion
   end
 
   private
+
+    def tag_uniqueness
+      is_duplicated = Tag.by_name(name).any? do |t|
+        if t.shared == true
+          true
+        elsif new_record? && (t.organization == organization || shared == true)
+          true
+        elsif persisted? && (t.organization == organization || shared == true && t.organization != organization)
+          true
+        else
+          false
+        end
+      end
+
+      errors.add :name, :taken if is_duplicated
+    end
 
     def shared_reversion
       errors.add :shared, :invalid if shared_was && !shared
