39c46343a5c6c40c471bb22af156bb90359a50db
Ismael Isuani <ismaelisuani@gmail.com>
2023-10-15 14:39:11 -0300

    Improved heatmap

--- app/models/concerns/risk_assessments/heatmap.rb
+++ app/models/concerns/risk_assessments/heatmap.rb
@@ -12,7 +12,7 @@ module RiskAssessments::Heatmap
       hsh[:body][rsix.name] = []
 
       rwhy.risk_score_items.ordered.each do |rsiy|
-        hsh[:body][rsix.name] << risk_weights_for(rsix.value, rsiy.value)
+        hsh[:body][rsix.name] << risk_weights_for(rsix, rsiy)
       end
     end
 
@@ -30,10 +30,22 @@ module RiskAssessments::Heatmap
 
   private
 
-    def risk_weights_for valuex, valuey
-      rw_group = RiskWeight.joins(:risk_assessment_weight).select('risk_assessment_item_id').where(
-        'heatmap IS TRUE AND (value = :valuex OR value = :valuey)', valuex: valuex, valuey: valuey
-      ).group('risk_assessment_item_id').having('count(*) = 2')
+    def risk_weights_for rsix, rsiy
+      rw_group = RiskWeight.joins(
+        :risk_assessment_weight
+      ).select(
+        'risk_assessment_item_id'
+      ).where(
+        'heatmap IS TRUE AND
+        (risk_assessment_weight_id = :rawx_id AND value = :valuex) OR
+        (risk_assessment_weight_id = :rawy_id AND value = :valuey)',
+        rawx_id: rsix.risk_assessment_weight_id, valuex: rsix.value,
+        rawy_id: rsiy.risk_assessment_weight_id, valuey: rsiy.value
+      ).group(
+        'risk_assessment_item_id'
+      ).having(
+        'count(*) = 2'
+      )
 
       risk_assessment_items.where(id: rw_group).count
     end
