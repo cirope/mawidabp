1f8e29dbfb9463c3b25a3b808f0a300e1c91d1af
Sergio Palacios <asergiop21@gmail.com>
2023-11-02 21:29:30 -0300

    Add claim attributes to SAML config (#679)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_01_112101) do
+ActiveRecord::Schema.define(version: 2023_11_01_172300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -1224,6 +1224,11 @@ ActiveRecord::Schema.define(version: 2023_11_01_112101) do
     t.bigint "organization_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username_claim"
+    t.string "name_claim"
+    t.string "lastname_claim"
+    t.string "email_claim"
+    t.string "roles_claim"
     t.index ["default_role_for_users_id"], name: "index_saml_providers_on_default_role_for_users_id"
     t.index ["organization_id"], name: "index_saml_providers_on_organization_id"
   end
