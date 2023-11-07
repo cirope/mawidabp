55321dbc4105e644208162317e8a275dbcb66b94
Sergio <asergiop21@gmail.com>
2023-11-07 10:16:20 -0300

    Fix review

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,    'c-macro-test'
+set :branch,    'master'
 set :stage,     :production
 set :rails_env, 'production'
 
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@macro-test.mawidabp.com}
-role :app, %w{deployer@macro-test.mawidabp.com}
-role :db,  %w{deployer@macro-test.mawidabp.com}
+role :web, %w{deployer@app.mawidabp.com}
+role :app, %w{deployer@app.mawidabp.com}
+role :db,  %w{deployer@app.mawidabp.com}
 
-server 'macro-test.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
