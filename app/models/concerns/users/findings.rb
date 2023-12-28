c5063297cd4864bff0b86d15620745f5ebe02d60
Ismael Isuani <ismaelisuani@gmail.com>
2023-12-28 10:30:10 -0300

    Improved dashboard

--- app/models/concerns/users/findings.rb
+++ app/models/concerns/users/findings.rb
@@ -10,7 +10,7 @@ module Users::Findings
     has_many :oportunities, through: :finding_user_assignments,
       source_type: 'Oportunity', source: :finding
     has_many :tasks, through: :findings
-    has_many :finding_answers, through: :findings
+    has_many :finding_answers, -> { merge Finding.list }, through: :findings
   end
 
   def has_pending_findings?
