4a94379edcc014648e177c6d0ef575d2a187c8ba
Ismael Isuani <ismaelisuani@gmail.com>
2023-12-29 17:16:12 -0300

    Fix dashboard graph when empty count (#716)

--- app/models/concerns/findings/graph_helpers.rb
+++ app/models/concerns/findings/graph_helpers.rb
@@ -3,20 +3,27 @@ module Findings::GraphHelpers
 
   module ClassMethods
     def graph_findings findings
-      from_date = 11.months.ago.beginning_of_month.to_date
-      end_date  = Date.today
+      incomplete = latest_findings(findings).with_pending_status.count
+      completed  = latest_findings(findings).with_completed_status.count
 
-      latest_findings = findings.
-        where(origination_date: from_date..end_date).
-        group_by_month(:origination_date)
-
-      incomplete = latest_findings.with_pending_status.count
-      completed  = latest_findings.with_completed_status.count
+      incomplete = [] if incomplete.blank?
+      completed  = [] if completed.blank?
 
       [
         { name: I18n.t('findings.graphs.incomplete'), data: incomplete },
         { name: I18n.t('findings.graphs.completed'),  data: completed  }
       ]
     end
+
+    private
+
+      def latest_findings findings
+        from_date = 11.months.ago.beginning_of_month
+        end_date  = Date.today
+
+        latest_findings = findings.
+          where(origination_date: from_date..end_date).
+          group_by_month(:origination_date)
+      end
   end
 end
