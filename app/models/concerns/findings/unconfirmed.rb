333fc0e2b4db0b8346f804f54190bd88af6af435
Jonathan <villegas.jonathan88@gmail.com>
2024-04-16 06:50:03 -0300

    Modify logic to send multiple notifications to unconfirmed findings

--- app/models/concerns/findings/unconfirmed.rb
+++ app/models/concerns/findings/unconfirmed.rb
@@ -2,17 +2,17 @@ module Findings::Unconfirmed
   extend ActiveSupport::Concern
 
   included do
-    scope :unconfirmed_for_notification, -> {
+    scope :unconfirmed_for_notification, -> (days) {
       where(
         [
-          'first_notification_date >= :stale_unconfirmed_date',
+          'first_notification_date = :stale_unconfirmed_date',
           'state = :state',
           'final = :false'
         ].join(' AND '),
         {
           state: Finding::STATUS[:unconfirmed],
           false: false,
-          stale_unconfirmed_date: FINDING_DAYS_FOR_SECOND_NOTIFICATION.business_days.ago.to_date
+          stale_unconfirmed_date: days.business_days.ago.to_date
         }
       )
     }
@@ -20,19 +20,38 @@ module Findings::Unconfirmed
 
   module ClassMethods
     def notify_for_unconfirmed_for_notification_findings
-      # Sólo si no es sábado o domingo
       if Time.zone.today.workday?
-        Finding.transaction do
-          users = Finding.unconfirmed_for_notification.inject([]) do |u, finding|
-            u | finding.users.select do |user|
-              user.notifications.not_confirmed.any? do |n|
-                n.findings.include?(finding)
-              end
+        finding_days_for_next_notifications_parameters.each do |organization, value|
+          Current.organization = organization
+          Current.group        = organization.group
+          days_array           = value.to_s.split(',').map { |v| v.strip.to_i }
+
+          days_array.each do |days|
+            Finding.transaction do
+              findings = Finding.list.unconfirmed_for_notification(days)
+
+              notify findings, days
             end
           end
-
-          users.each { |user| NotifierMailer.stale_notification(user).deliver_later }
         end
+      end
+    end
+
+    private
+
+    def notify findings, days
+      users = findings.inject([]) do |u, finding|
+        u | finding.users.select do |user|
+          user.notifications.not_confirmed.any? { |n| n.findings.include?(finding) }
+        end
+      end
+
+      users.each { |user| NotifierMailer.stale_notification(user, days).deliver_later }
+    end
+
+    def finding_days_for_next_notifications_parameters
+      Organization.all_parameters('finding_days_for_next_notifications').map do |p|
+        [p[:organization], p[:parameter]]
       end
     end
   end
