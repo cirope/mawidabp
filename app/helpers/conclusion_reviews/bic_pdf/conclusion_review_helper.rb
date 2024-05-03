73a92860f79582a946ae7585c6f6a90addc0dda7
Jonathan <villegas.jonathan88@gmail.com>
2024-05-03 02:50:10 -0300

    Customize html formatting and sanitization for bic conclusion review

--- app/helpers/conclusion_reviews/bic_pdf/conclusion_review_helper.rb
+++ app/helpers/conclusion_reviews/bic_pdf/conclusion_review_helper.rb
@@ -137,6 +137,14 @@ module ConclusionReviews::BicPdf::ConclusionReviewHelper
     end
   end
 
+  def format_and_custom_sanitize input_text
+    formatted_text     = simple_format input_text, {}, sanitize: false
+    allowed_tags       = %w[b i em strong u br small sub sup mark p div span ul ol li]
+    sanitized_text     = sanitize formatted_text, tags: allowed_tags
+
+    raw sanitized_text
+  end
+
   private
 
     def base_weaknesses conclusion_review
