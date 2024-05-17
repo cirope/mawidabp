f9433cfe30c0517a267be6c51eb7b43d049ed7c2
Jonathan <villegas.jonathan88@gmail.com>
2024-05-17 02:33:13 -0300

    Fix findings_expiration_warning mail

--- app/helpers/notifier_helper.rb
+++ app/helpers/notifier_helper.rb
@@ -1,6 +1,9 @@
 module NotifierHelper
   def finding_audited_names(finding)
-    finding.users.select(&:can_act_as_audited?).map do |user|
+    finding.users.select do |user|
+      corporate_organization_ids = user.organizations.corporate.map(&:id)
+      user.can_act_as_audited? || corporate_organization_ids.any? { |id| user.can_act_as_audited_on?(id) }
+    end.map do |user|
       if finding.process_owners.include? user
         content_tag :b,
           "#{user.full_name} (#{FindingUserAssignment.human_attribute_name('process_owner')})"
