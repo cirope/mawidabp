f4b9412e16fa53cc75bd32043406a758352f7341
Ismael Isuani <ismaelisuani@gmail.com>
2024-09-12 19:07:55 -0300

    Rollback WickedPdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,4 @@
-WickedPdf.configure do |c|
-  c.exe_path                 = '/usr/bin/wkhtmltopdf'
-  c.enable_local_file_access = true
-end
+WickedPdf.config = {
+  exe_path: '/usr/bin/wkhtmltopdf',
+  enable_local_file_access: true
+}
