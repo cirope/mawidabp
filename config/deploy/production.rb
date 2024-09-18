2fa52eb0e099d658367b3be9209ea94e848e9105
Sergio <asergiop21@gmail.com>
2024-09-18 17:23:56 -0300

    Setup initial

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,    'master'
+set :branch,    'c-btf-test'
 set :stage,     :production
 set :rails_env, 'production'
 
@@ -8,8 +8,8 @@ set :default_env, {
   'LC_ALL':   'en_US.UTF-8'
 }
 
-role :web, %w{deployer@app.mawidabp.com}
-role :app, %w{deployer@app.mawidabp.com}
-role :db,  %w{deployer@app.mawidabp.com}
+role :web, %w{deployer@btf-test.mawidabp.com}
+role :app, %w{deployer@btf-test.mawidabp.com}
+role :db,  %w{deployer@btf-test.mawidabp.com}
 
-server 'app.mawidabp.com', user: 'deployer', roles: %w{web app db}
+server 'btf-test.mawidabp.com', user: 'deployer', roles: %w{web app db}
