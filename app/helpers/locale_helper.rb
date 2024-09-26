2f6cf68b256a1bf26c341114599c5dba04e0833c
Jonathan <villegas.jonathan88@gmail.com>
2024-09-26 12:23:22 -0300

    Use local I18n.available instead of constant

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -1,6 +1,6 @@
 module LocaleHelper
   def language_options
-    AVAILABLE_LOCALES.map do |lang|
+    I18n.available_locales.map do |lang|
       [I18n.t("lang.#{lang}"), lang.to_s]
     end.sort { |a, b| a[0] <=> b[0] }
   end
