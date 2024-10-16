e6adb262ddc6862e8f457380348a496e90a3e31a
Jonathan <villegas.jonathan88@gmail.com>
2024-10-16 13:37:59 -0300

    Remove semicolons

--- app/assets/javascripts/locale.js
+++ app/assets/javascripts/locale.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const localeSelector = document.querySelector('[data-locale-selector]');
+  const localeSelector = document.querySelector('[data-locale-selector]')
 
   if (localeSelector) {
     localeSelector.addEventListener('change', (event) => {
-      changeLocale(event.target.value);
-    });
+      changeLocale(event.target.value)
+    })
   }
-});
+})
 
 function changeLocale(locale) {
-  const url = new URL(window.location.href);
+  const url = new URL(window.location.href)
 
-  url.searchParams.set('locale', locale);
-  window.location.href = url.toString();
+  url.searchParams.set('locale', locale)
+  window.location.href = url.toString()
 }
