575447c4061a6c0473fb01d3b1226505e66d7503
Sergio <asergiop21@gmail.com>
2024-10-21 17:19:01 -0300

    Validate keys

--- app/models/concerns/parameters/risk.rb
+++ app/models/concerns/parameters/risk.rb
@@ -2,14 +2,21 @@ module Parameters::Risk
   extend ActiveSupport::Concern
 
   RISK_TYPES = {
-    low: 0,
+    low:    0,
     medium: 1,
-    high: 2
+    high:   2
   }
 
   module ClassMethods
     def risks
       types = JSON.parse ENV['RISK_TYPES'] || '{}'
+
+      types.each do |key, _|
+        unless I18n.exists?("risk_types.#{key}")
+          errors.add :base,
+            I18n.t('')
+        end
+      end
 
       RISK_TYPES.merge! types.symbolize_keys
     end
