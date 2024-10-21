3693c1dcebb4a518511e6933f726a118f757c16e
Ismael Isuani <ismaelisuani@gmail.com>
2024-10-21 13:07:16 -0300

    Notify on finding state changes (#885)

--- test/models/finding_test.rb
+++ test/models/finding_test.rb
@@ -2722,6 +2722,18 @@ class FindingTest < ActiveSupport::TestCase
     assert_nil finding.next_task_expiration
   end
 
+  test 'should notify when finding state changed' do
+    finding = findings :being_implemented_weakness
+
+    finding.organization.settings.find_by(
+      name: 'finding_state_change_notification'
+    ).update! value: '1'
+
+    assert_enqueued_emails 1 do
+      finding.update! state: Finding::STATUS[:implemented]
+    end
+  end
+
   private
 
     def new_email_pop3 from, subject, body
