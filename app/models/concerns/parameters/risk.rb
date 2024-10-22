fd63e6ffe987c85be154bbf4c919707f756a18d2
Sergio <asergiop21@gmail.com>
2024-10-22 15:32:52 -0300

    Modfy FINDING_RISK_TYPES const

--- app/models/concerns/parameters/risk.rb
+++ app/models/concerns/parameters/risk.rb
@@ -13,8 +13,6 @@ module Parameters::Risk
 
   module ClassMethods
     def risks
-      raise 'Risk configuration error' unless valid_risk_types?
-
       RISK_TYPES
     end
 
@@ -29,21 +27,25 @@ module Parameters::Risk
     private
 
       def risk_types
-        finding_risk_types = JSON.parse ENV['FINDING_RISK_TYPES'] || '{}'
+        raise 'Risk configuration error' unless valid_risk_types?
 
-        if finding_risk_types.present?
-          finding_risk_types.symbolize_keys
+        risk_types = JSON.parse ENV['RISK_TYPES'] || '{}'
+
+        if risk_types.present?
+          risk_types.symbolize_keys
         else
           DEFAULT_RISK_TYPES
         end
       end
 
       def valid_risk_types?
-        risk_types        = RISK_TYPES.keys
-        risk_types_values = RISK_TYPES.values.tally.select { |_, count| count > 1 }.keys
+        risk_types = JSON.parse ENV['RISK_TYPES'] || '{}'
+
+        risk_types_keys   = risk_types.symbolize_keys.keys
+        risk_types_values = risk_types.values.uniq.size == risk_types.values.size
         i18n_risk_types   = I18n.translate('risk_types').keys
 
-        (risk_types - i18n_risk_types).blank? && risk_types_values.blank?
+        (risk_types_keys - i18n_risk_types).blank? && risk_types_values
       end
   end
 end
