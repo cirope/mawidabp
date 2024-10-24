58b4649ab4f38ae4e441e3e30be4b85005dbc0cc
Jonathan Villegas <villegas.jonathan88@gmail.com>
2024-10-24 17:28:41 -0300

    Merges and normalizes dino i18n (#843)

--- app/models/concerns/users/licenses.rb
+++ app/models/concerns/users/licenses.rb
@@ -3,16 +3,16 @@ module Users::Licenses
   extend ActiveSupport::Concern
 
   included do
-    before_save :check_auditors_limit_on_role_change, if: :check_auditors_limit?
+    before_save :check_auditors_limit_on_role_or_enable_change, if: :check_auditors_limit?
   end
 
   private
 
     def check_auditors_limit?
-      new_record? || roles_has_changed?
+      new_record? || roles_has_changed? || enable_changed?
     end
 
-    def check_auditors_limit_on_role_change
+    def check_auditors_limit_on_role_or_enable_change
       if adding_new_auditor? && !Current.group.can_create_auditor?
         Rails.logger.info "Group auditors limit reached (#{Current.group.auditors_limit})"
         errors.add :base, :auditors_limit_reached
@@ -27,8 +27,10 @@ module Users::Licenses
       organization_roles.reject(&:marked_for_destruction?).any? do |organization_role|
         audited = if new_record?
                     true
+                  elsif enable_changed?
+                    !old_user.enable
                   else
-                    old_user.can_act_as_audited_on? organization_role.organization_id
+                    old_user.can_act_as_audited? organization_role.organization_id
                   end
 
         audited && Role::ACT_AS[:auditor].include?(organization_role.role.role_type)
