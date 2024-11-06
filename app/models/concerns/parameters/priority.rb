2a51d95f747fb165fdd889a36b12fdc3484fbb41
Sergio <asergiop21@gmail.com>
2024-11-06 09:37:16 -0300

    Priority types refactor

--- app/models/concerns/parameters/priority.rb
+++ app/models/concerns/parameters/priority.rb
@@ -5,6 +5,12 @@ module Parameters::Priority
     ::PRIORITY_TYPES = priority_types unless defined? ::PRIORITY_TYPES
   end
 
+  DEFAULT_PRIORITY_TYPES = {
+    low: 0,
+    medium: 1,
+    high: 2
+  }
+
   module ClassMethods
     def priorities
       PRIORITY_TYPES
@@ -17,13 +23,23 @@ module Parameters::Priority
     private
 
       def priority_types
-        if USE_SCOPE_CYCLE
-          { none: 0, low: 1, medium: 2, high: 3 }
-        elsif SHOW_CONDENSED_PRIORITIES
-          { low: 0, high: 2 }
+        priority_types = JSON.parse ENV['PRIORITY_TYPES'] || '{}'
+
+        raise 'Priority configuration error' unless valid_priority_types? priority_types
+
+        if priority_types.present?
+          priority_types.symbolize_keys
         else
-          { low: 0, medium: 1, high: 2 }
+          DEFAULT_PRIORITY_TYPES
         end
       end
-  end
+
+      def valid_priority_types? priority_types
+        priority_types_keys    = priority_types.symbolize_keys.keys
+        unique_priority_values = priority_types.values.uniq.size == priority_types.values.size
+        i18n_priority_types    = I18n.translate('priority_types').keys
+
+        (priority_types_keys - i18n_priority_types).blank? && unique_priority_values
+      end
+    end
 end
