516b400705cd99079c0f9a5fa3a856819fd7bf97
Ismael Isuani <ismaelisuani@gmail.com>
2024-11-26 14:21:32 -0300

    Cleaner work papers helper (#921)

--- app/helpers/work_papers_helper.rb
+++
@@ -1,9 +0,0 @@
-module WorkPapersHelper
-  def work_paper_show_change_history element_id
-    link_to icon('fas', 'history'), "##{element_id}", {
-      title: t('work_papers.history.show'),
-      data:  { bs_toggle: 'collapse' },
-      class: 'me-4'
-    }
-  end
-end
