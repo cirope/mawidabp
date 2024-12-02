c3481448cb237c23c030ebb1c28fb08a0873847c
Sergio <asergiop21@gmail.com>
2024-12-02 15:37:40 -0300

    Suggestion

--- app/models/concerns/control_objective_items/history.rb
+++ app/models/concerns/control_objective_items/history.rb
@@ -5,7 +5,7 @@ module ControlObjectiveItems::History
     versions.each_with_object([]) do |version, result|
       coi = version.reify has_one: false
 
-      if coi&.auditor_comment && coi&.auditor_comment != auditor_comment
+      if coi&.auditor_comment && coi.auditor_comment != auditor_comment
         date    = I18n.l version.created_at, format: :long
         user    = User.find_by id: version.whodunnit
         action  = I18n.t "control_objective_items.history.actions.#{version.event}"
