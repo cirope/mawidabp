7fb07d6b36382a826fbe07066285899aa8486b28
Sergio Palacios <asergiop21@gmail.com>
2024-12-04 15:20:30 -0300

    Improve change history in auditor_comment(#930)

--- app/models/concerns/control_objective_items/history.rb
+++ app/models/concerns/control_objective_items/history.rb
@@ -13,8 +13,7 @@ module ControlObjectiveItems::History
         history = {
           date:   date.strip,
           user:   user.informal_name,
-          action: action,
-          change: coi.auditor_comment
+          action: action
         }
 
         result << history
