bd67c9676ad38d86d0c1d0bcc29ab6a46461e176
Ismael Isuani <ismaelisuani@gmail.com>
2024-12-20 10:46:01 -0300

    Added planned risk to conclusion reviews

--- app/models/concerns/conclusion_reviews/risks.rb
+++ app/models/concerns/conclusion_reviews/risks.rb
@@ -1,9 +1,17 @@
 module ConclusionReviews::Risks
   extend ActiveSupport::Concern
 
-  def risk_text
+  def risk_text value: risk
     Current.organization.risks_text_for(
-      date: created_at, value: risk
+      date: created_at, value: value
     )
+  end
+
+  def planned_risk
+    if risk = review.plan_item.risk_assessment_item&.risk
+      risk_text(value: risk) || I18n.t('conclusion_reviews.risks.unknown')
+    else
+      I18n.t 'conclusion_reviews.risks.none'
+    end
   end
 end
